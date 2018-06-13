c54f65aa2f577fbce64916ae56cf6bdd6bb143ce
Rich143 <richard.arnold.matthews@gmail.com>
2018-06-12 20:20:10 -0700

    Working state machine

--- Inc/state_machine.h
+++ Inc/state_machine.h
@@ -1,6 +1,8 @@
 #ifndef __STATE_MACHINE_H
 #define __STATE_MACHINE_H
 #include "bsp.h"
+#include "freertos.h"
+#include "queue.h"
 
 // To use this state machine framework, implement an enum of states, an enum of
 // events, and a transition table in your code
@@ -25,14 +27,27 @@ typedef struct {
     uint32_t ST_ANY;
     uint32_t EV_ANY;
     Transition_t *transitions;
+    uint32_t eventQueueLength;
 } FSM_Init_Struct;
+
+typedef struct {
+    FSM_Init_Struct init;
+    QueueHandle_t eventQueue;
+    uint32_t state;
+} FSM_Handle_Struct;
+
 
 // Get the size of a transition table, only work when passing in the original
 // transition table array, not a pointer to it
 #define TRANS_COUNT(transition_table) \
     (sizeof(transition_table)/sizeof(*transition_table))
 
-HAL_StatusTypeDef fsmInit(uint32_t startingState, FSM_Init_Struct *init);
-HAL_StatusTypeDef fsmProcessEvent(uint32_t event);
+HAL_StatusTypeDef fsmInit(uint32_t startingState, FSM_Init_Struct *init,
+                          FSM_Handle_Struct *handle);
+void fsmTaskFunction(FSM_Handle_Struct *handle);
 uint32_t fsmGetState();
+HAL_StatusTypeDef fsmSendEventUrgent(FSM_Handle_Struct *handle, uint32_t event, uint32_t timeout_ms);
+HAL_StatusTypeDef fsmSendEvent(FSM_Handle_Struct *handle, uint32_t event, uint32_t timeout_ms);
+HAL_StatusTypeDef fsmSendEventUrgentISR(FSM_Handle_Struct *handle, uint32_t event);
+HAL_StatusTypeDef fsmSendEventISR(FSM_Handle_Struct *handle, uint32_t event);
 #endif // __STATE_MACHINE_H
