361baa24babd3300bce60a6e22cd563fea9e3fce
Rich143 <richard.arnold.matthews@gmail.com>
2018-07-19 20:47:35 -0700

    Add print statement to test uart

--- Src/mainTaskEntry.c
+++ Src/mainTaskEntry.c
@@ -2,10 +2,13 @@
 #include "task.h"
 
 #include "bsp.h"
+#include "debug.h"
 
 void mainTaskFunction(void const * argument)
 {
+    printf("Starting up!!\n");
     while (1) {
+        printf("Hello\n");
         HAL_GPIO_TogglePin(DEBUG_LED_PORT, DEBUG_LED_PIN);
 
         vTaskDelay(1000);
