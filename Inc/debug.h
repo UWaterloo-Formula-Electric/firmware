f341910b25bc77f7f620ea35da9ea05e8117d62d
Rich143 <richard.arnold.matthews@gmail.com>
2018-11-14 14:16:52 +0100

    Switch to use FreeRTOS CLI

--- Inc/debug.h
+++ Inc/debug.h
@@ -20,10 +20,18 @@
 #define ERROR_PRINT(...)
 #endif
 
-#define UART_PRINT_TIMEOUT 100
+#ifdef CONSOLE_PRINT_ON
+#define CONSOLE_PRINT(...) _DEBUG_PRINT(__VA_ARGS__)
+#else
+#define CONSOLE_PRINT
+#endif
 
+/*
+ * Send Function and Defines
+ */
+#define UART_PRINT_TIMEOUT 100
 #define PRINT_QUEUE_LENGTH 15
-#define PRINT_QUEUE_STRING_SIZE 80
+#define PRINT_QUEUE_STRING_SIZE 100
 #define PRINT_QUEUE_SEND_TIMEOUT_TICKS  10
 
 /**
@@ -73,6 +81,27 @@
     } while(0)
 
 extern QueueHandle_t printQueue;
+
+/* 
+ * Receive and CLI functions and defines
+ *
+ */
+// This is the size of the buffer used by FreeRTOS+CLI to write command output
+// to
+// this **NEEDS* to be the same as the print queue string size, as the console
+// send function relies on this. This optimizes sending
+#define configCOMMAND_INT_MAX_OUTPUT_SIZE PRINT_QUEUE_STRING_SIZE
+
+#define UART_RX_QUEUE_LENGTH 20
+
+#define STR_EQ(a, b, len) (strncmp(a, b, len) == 0)
+
+// Output to the command output buffer, can only be called once per command
+// function
+#define COMMAND_OUTPUT(...)  \
+    do { \
+        snprintf(writeBuffer, writeBufferLength, __VA_ARGS__); \
+    } while(0)
 
 HAL_StatusTypeDef debugInit();
 HAL_StatusTypeDef uartStartReceiving(UART_HandleTypeDef *huart);
