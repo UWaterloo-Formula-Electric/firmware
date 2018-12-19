b3a84a3f0f2a8b7b699067f696c6fe65a41c36fe
Rich143 <richard.arnold.matthews@gmail.com>
2018-12-19 16:27:43 +0100

    Fix Cube-Lib.mk for Nucleo to have correct filename for hal_timebase_TIM.c

--- Cube-Nucleo-Src/CanTest/Cube-Lib.mk
+++ Cube-Nucleo-Src/CanTest/Cube-Lib.mk
@@ -58,7 +58,7 @@ LIB_C_SOURCES =  \
 ../Src/dma.c \
 ../Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_dma_ex.c \
 ../Src/freertos.c \
-../Src/stm32f7xx_hal_timebase_tim.c \
+../Src/stm32f7xx_hal_timebase_TIM.c \
 ../Middlewares/Third_Party/FreeRTOS/Source/event_groups.c \
 ../Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_rcc.c \
 ../Middlewares/Third_Party/FreeRTOS/Source/tasks.c \
