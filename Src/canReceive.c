ef1e88b9afe024e0fc5ba9b8add40b4f16c97f0b
Rich143 <richard.arnold.matthews@gmail.com>
2019-05-31 03:40:50 -0400

    Control AMS and IMD LEDs

--- Src/canReceive.c
+++ Src/canReceive.c
@@ -9,6 +9,7 @@
 
 #include "freertos.h"
 #include "cmsis_os.h"
+#include "DCU_dtc.h"
 
 extern osThreadId mainTaskHandle;
 
@@ -51,3 +52,17 @@ void CAN_Msg_VCU_EM_State_Callback()
                             NULL );
     }
 }
+
+void CAN_Msg_BMU_DTC_Callback(int DTC_CODE, int DTC_Severity, int DTC_Data)
+{
+    if (DTC_CODE == FATAL_IMD_Failure) {
+        ERROR_PRINT_ISR("Got IMD failure\n");
+        IMD_FAIL_LED_ON
+    } else if (DTC_CODE == CRITICAL_CELL_VOLTAGE_LOW
+               || DTC_CODE == CRITICAL_CELL_VOLTAGE_HIGH
+               || DTC_CODE == CRITICAL_CELL_TEMP_HIGH) {
+        ERROR_PRINT_ISR("Got AMS failure\n");
+        AMS_FAIL_LED_ON
+    }
+}
+
