2f6fa867d5d58e8ba19e261a299eefa720358302
Rich143 <richard.arnold.matthews@gmail.com>
2019-05-31 03:43:03 -0400

    Add in bsp for AMS contactor, and defines for cell temp, and make map range public api

--- Inc/batteries.h
+++ Inc/batteries.h
@@ -6,6 +6,14 @@
 #include "queue.h"
 #include "bsp.h"
 
+#define CELL_TIME_TO_FAILURE_ALLOWABLE (6.0)
+#define CELL_DCR (0.01)
+#define CELL_HEAT_CAPACITY (1034.2) //kj/kg•k
+#define CELL_MASS (0.496)
+#define CELL_MAX_TEMP_C (60.0)
+#define CELL_OVERTEMP (CELL_MAX_TEMP_C)
+#define CELL_OVERTEMP_WARNING (CELL_MAX_TEMP_C - 10)
+
 typedef enum Charge_Notifications_t {
     CHARGE_START_NOTIFICATION,
     CHARGE_STOP_NOTIFICATION,
@@ -19,5 +27,6 @@ HAL_StatusTypeDef initBusVoltagesAndCurrentQueues();
 HAL_StatusTypeDef balance_cell(int cell, bool set);
 HAL_StatusTypeDef getPackVoltage(float *packVoltage);
 HAL_StatusTypeDef initPackVoltageQueue();
+float map_range_float(float in, float low, float high, float low_out, float high_out);
 
 #endif /* end of include guard: BATTERIES_H */
