6b58d4a6f468aa507d2dfb4d93d777ae008a0a06
Rich143 <richard.arnold.matthews@gmail.com>
2019-06-01 10:57:45 -0400

    Increase watchdog timeout and countdown

--- Cube-F0-Src/DCU/Src/iwdg.c
+++ Cube-F0-Src/DCU/Src/iwdg.c
@@ -63,7 +63,7 @@ void MX_IWDG_Init(void)
   hiwdg.Instance = IWDG;
   hiwdg.Init.Prescaler = IWDG_PRESCALER_4;
   hiwdg.Init.Window = 4095;
-  hiwdg.Init.Reload = 200;
+  hiwdg.Init.Reload = 400;
   if (HAL_IWDG_Init(&hiwdg) != HAL_OK)
   {
     Error_Handler();
