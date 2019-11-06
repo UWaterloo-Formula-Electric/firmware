b2d47d8949a73fcfddb5d4670e03f3c46926b0b8
Rich143 <richard.arnold.matthews@gmail.com>
2019-11-06 18:29:53 -0500

    Initial commit for working build

---
+++ Makefile
@@ -0,0 +1,14 @@
+BINARY_BASE_NAME=WSB
+BOARD_NAME=WSBFL
+
+COMMON_LIB_SRC = userCan.c debug.c state_machine.c CRC_CALC.c FreeRTOS_CLI.c freertos_openocd_hack.c watchdog.c generalErrorHandler.c
+COMMON_F0_LIB_SRC = userCanF0.c
+
+# one of NUCLEO_F7, F7, NUCLEO_F0, F0
+BOARD_TYPE=F0
+
+CUBE_F0_MAKEFILE_PATH= Cube-F0-Src/2018_WSB/
+CUBE_NUCLEO_F0_MAKEFILE_PATH = Cube-Nucleo-Src/WSB/
+
+
+include common-all/tail.mk
---
+++ Src/userInit.c
@@ -0,0 +1,36 @@
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "bsp.h"
+#include "debug.h"
+#include "userCan.h"
+
+void vApplicationStackOverflowHook( TaskHandle_t xTask,
+                                    signed char *pcTaskName )
+{
+    HAL_GPIO_WritePin(ERROR_LED_PORT, ERROR_LED_PIN, GPIO_PIN_SET);
+    printf("Stack overflow for task %s\n", pcTaskName);
+}
+
+
+// This is declared with weak linkage in all Cube main.c files, and called
+// before freeRTOS initializes and starts up
+void userInit()
+{
+    /* Should be the first thing initialized, otherwise print will fail */
+    if (debugInit() != HAL_OK) {
+        Error_Handler();
+    }
+
+    if (uartStartReceiving(&DEBUG_UART_HANDLE) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    if (canInit(&CAN_HANDLE) != HAL_OK) {
+      Error_Handler();
+    }
+
+    printf("User init done\n");
+}
+
