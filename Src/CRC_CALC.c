280ffcd7e1629b549bf06448a5a8353f9139105a
Zulaikha Zakiullah <zzakiull@uwaterloo.ca>
2020-06-17 23:05:31 +0000

    CRC_CALC.c edited online with Bitbucket

--- Src/CRC_CALC.c
+++ Src/CRC_CALC.c
@@ -1,24 +1,9 @@
 #include "bsp.h"
 
-CRC_HandleTypeDef crc_handle;
 const uint32_t BUFFER_SIZE = (uint32_t)(6);
 
 uint8_t calculate_base_CRC( uint8_t * data_bytes )
 {
-    uint8_t crc_value = (uint8_t)HAL_CRC_Calculate(&crc_handle, (uint32_t*)(data_bytes), BUFFER_SIZE);
+    uint8_t crc_value = (uint8_t)HAL_CRC_Calculate(&hcrc, (uint32_t*)(data_bytes), BUFFER_SIZE);
     return (uint8_t)(crc_value ^ 0xff);
-}
-
-void init_crc_handle()
-{
-    crc_handle.Instance = CRC;
-    crc_handle.Init.DefaultPolynomialUse = DEFAULT_POLYNOMIAL_DISABLE;
-    crc_handle.Init.GeneratingPolynomial = 0x2f;
-    crc_handle.Init.CRCLength = CRC_POLYLENGTH_8B;
-    crc_handle.InputDataFormat = CRC_INPUTDATA_FORMAT_BYTES;
-    
-    if (HAL_CRC_Init(&crc_handle) != HAL_OK)
-    {
-        Error_Handler();
-    }
 }
