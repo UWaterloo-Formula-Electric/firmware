92aa1370d62028eb4b275eb0b106641f88f6133c
Desmond Good <djgood@uwaterloo.ca>
2020-10-15 22:41:55 -0400

    WFE-565 package/libopenblt: Initialize package config

---
+++ package/libopenblt/libopenblt.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# libopenblt
+#
+################################################################################
+
+LIBOPENBLT_VERSION = 1.3.4
+LIBOPENBLT_SOURCE = openblt_v011000.tar.gz
+LIBOPENBLT_SITE = https://github.com/feaser/openblt/archive
+
+$(eval $(cmake-package))
+
