da1cec2684b50af3f87f542e51815944852da313
Desmond Good <djgood@uwaterloo.ca>
2020-10-16 03:14:50 +0000

    WFE-565 package/libopenblt: add package to BR2 config
    Add the OpenBLT package and include in the default defconfig for our
    project.
    
    OpenBLT is the library used for communicating with the custom CAN
    bootloaders installed on the STM32F072RB and STM32F767VI chips included
    in the various subsustems of the vehicle.
    
    This library has Python bindings and will be used by a Python script
    added in the beaglebone application package.
    
    Select BR2_PACKAGE_LIBOPENBLT to enable.
    
    Also, intializes adding project packages to our BR2_EXTERNAL tree
    structure.

--- package/libopenblt/libopenblt.mk
+++ package/libopenblt/libopenblt.mk
@@ -7,6 +7,7 @@
 LIBOPENBLT_VERSION = 1.3.4
 LIBOPENBLT_SOURCE = openblt_v011000.tar.gz
 LIBOPENBLT_SITE = https://github.com/feaser/openblt/archive
+LIBOPENBLT_DEPENDENCIES = libusb libsocketcan
 
 $(eval $(cmake-package))
 
