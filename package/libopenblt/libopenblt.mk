d74b7e4a9cca8f069ea2e026e86a8a11eb7eaf06
Desmond Good <djgood@uwaterloo.ca>
2020-10-24 17:53:21 +0000

    WFE-565 package/libopenblt: install libopenblt.so to /usr/lib
    Add a hook to the Makefile to copy the output from the build process to
    /usr/lib on the target so that the library may be accessed when running
    on the Beaglebone.

--- package/libopenblt/libopenblt.mk
+++ package/libopenblt/libopenblt.mk
@@ -6,8 +6,13 @@
 
 LIBOPENBLT_VERSION = 1.3.4
 LIBOPENBLT_SOURCE = openblt_v011000.tar.gz
-LIBOPENBLT_SITE = https://github.com/feaser/openblt/archive
+LIBOPENBLT_SITE = https://github.com/feaser/openblt
+LIBOPENBLT_INSTALL_TARGET = YES
 LIBOPENBLT_DEPENDENCIES = libusb libsocketcan
+
+define LIBOPENBLT_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/build/libopenblt.so $(TARGET_DIR)/usr/lib
+enddef
 
 $(eval $(cmake-package))
 
