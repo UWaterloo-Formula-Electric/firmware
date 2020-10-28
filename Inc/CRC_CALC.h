086dcd3873cb2e13b41861c36513be6417458f26
Zulaikha Zakiullah <zzakiull@uwaterloo.ca>
2020-10-28 18:29:58 -0400

    Declare HAL_CRC_Calculate only if HAL CRC peripheral is defined

--- Inc/CRC_CALC.h
+++ Inc/CRC_CALC.h
@@ -1,3 +1,5 @@
+#ifdef __STM32F7xx_HAL_CRC_H
+
 #ifndef __CRC_CALC_H
 #define __CRC_CALC_H
 
@@ -5,3 +7,5 @@ uint8_t calculate_base_CRC( uint8_t * data_bytes );
 void generate_CRC_lookup_table();
 
 #endif /* defined(__CRC_CALC_H)*/
+
+#endif /* defined(__STM32F7xx_HAL_CRC_H) */
