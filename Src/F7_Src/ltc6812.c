f4655fe8e55875853cd8157891b1b8a473bae313
123p10 <obrake3@gmail.com>
2021-03-06 16:52:22 -0500

    Added LTC6812 bytes

--- Src/F7_Src/ltc6812.c
+++ Src/F7_Src/ltc6812.c
@@ -22,9 +22,9 @@
 #include "task.h"
 
 // The following defines are always fixed due to AMS architecture, DO NOT CHANGE
-#define VOLTAGE_BLOCKS_PER_BOARD    4   // Number of voltage blocks per AMS board
+#define VOLTAGE_BLOCKS_PER_BOARD    5   // Number of voltage blocks per AMS board
 #define VOLTAGES_PER_BLOCK          3   // Number of voltage reading per block
-#define TEMP_CHANNELS_PER_BOARD     12
+#define TEMP_CHANNELS_PER_BOARD     16
 #define VOLTAGE_MEASURE_DELAY_MS    2   // Length of time for voltage measurements to finish
 #define VOLTAGE_MEASURE_DELAY_EXTRA_US 400 // Time to add on to ms delay for measurements to finsh
 #define TEMP_MEASURE_DELAY_US 405 // Time for measurements to finsh
@@ -97,11 +97,17 @@
 
 #else // defined(USE_ADDRES_MODE)
 
-#define WRCFG_BYTE0 (0x00)
-#define WRCFG_BYTE1 0x01
+#define WRCFGA_BYTE0 (0x00)
+#define WRCFGA_BYTE1 0x01
 
-#define RDCFG_BYTE0 (0x00)
-#define RDCFG_BYTE1 0x02
+#define WRCFGB_BYTE0 (0x00)
+#define WRCFGB_BYTE1 0x24
+
+#define RDCFGA_BYTE0 (0x00)
+#define RDCFGA_BYTE1 0x02
+
+#define RDCFGB_BYTE0 (0x00)
+#define RDCFGB_BYTE1 0x16
 
 #define RDCVA_BYTE0 (0x00)
 #define RDCVA_BYTE1 0x04
@@ -114,6 +120,9 @@
 
 #define RDCVD_BYTE0 (0x00)
 #define RDCVD_BYTE1 0x0a
+
+#define RDCVE_BYTE0 (0x00)
+#define RDCVE_BYTE1 0x09
 
 #define PLADC_BYTE0  (0x07)
 #define PLADC_BYTE1 0x14
@@ -211,8 +220,8 @@ static uint8_t m_batt_config[NUM_BOARDS][BATT_CONFIG_SIZE] = {0};
 // Convert from a channel to mux input to get that channel
 // There are acutally 13 temp channels on the board, but in software
 // we only use 12 for now
-uint8_t thermistorChannelToMuxLookup[TEMP_CHANNELS_PER_BOARD+1] = {
-    0xA, 0x9, 0x8, 0x7, 0x6, 0x5, 0x4, 0x2, 0x3, 0x0, 0x1, 0xB, 0xC };
+uint8_t thermistorChannelToMuxLookup[TEMP_CHANNELS_PER_BOARD] = {
+    0xA, 0x9, 0x8, 0x7, 0x6, 0x5, 0x4, 0x2, 0x3, 0x0, 0x1, 0xB, 0xC, 0xD, 0xE, 0xF };
 
 
 /* HSPI send/receive function */
@@ -628,6 +637,9 @@ HAL_StatusTypeDef batt_readBackCellVoltage(float *cell_voltage_array)
         {
             cmdByteLow = RDCVD_BYTE0;
             cmdByteHigh = RDCVD_BYTE1;
+        } else if(block == 4) {
+            cmdByteLow = RDCVE_BYTE0;
+            cmdByteHigh = RDCVE_BYTE1;
         } else {
             ERROR_PRINT("Attempt to access unkown voltage block\n");
             return HAL_ERROR;
