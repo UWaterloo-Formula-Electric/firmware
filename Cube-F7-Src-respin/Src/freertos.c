252d9215a66462ee4d0414a65db8733dd1287044
123p10 <obrake3@gmail.com>
2021-04-10 15:47:14 -0400

    ask5 -> 5 in 2018_vcu regen

--- Cube-F7-Src-respin/Src/freertos.c
+++ Cube-F7-Src-respin/Src/freertos.c
@@ -25,7 +25,7 @@
 #include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */     
+/* USER CODE BEGIN Includes */
 #include "stm32f7xx_hal.h"
 
 /* USER CODE END Includes */
