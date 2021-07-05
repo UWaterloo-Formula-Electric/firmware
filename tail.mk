b260714ee7d37bb06c5de49c93b60131b10ae4bd
Zulaikha Zakiullah <zzakiull@uwaterloo.ca>
2021-07-05 19:22:44 -0400

    Try env vars

--- tail.mk
+++ tail.mk
@@ -1,3 +1,5 @@
+OPENOCD_RUNNING = false
+
 CC=arm-none-eabi-gcc
 HEX=arm-none-eabi-objcopy
 SZ = arm-none-eabi-size
@@ -32,11 +34,6 @@ BOARD_VERSION ?= 1
 #alias these for boards that only have one version
 CUBE_F7_VERSION_1_MAKEFILE_PATH ?= $(CUBE_F7_MAKEFILE_PATH)
 CUBE_NUCLEO_F7_VERSION_1_MAKEFILE_PATH ?= $(CUBE_NUCLEO_MAKEFILE_PATH)
-
-# Check and warn about running instances of OpenOCD
-#ifeq ()
-#   $(warn "OpenOCD is already running")
-#endif
 
 ifeq ($(BOARD_TYPE), NUCLEO_F7)
    ifeq ($(BOARD_VERSION), 2)
@@ -310,21 +307,23 @@ else
 endif
 
 load: release
-#	openocd -f interface/stlink-v2-1.cfg -f $(OPENOCD_FILE) -c init -c "reset halt" -c halt -c "flash write_image erase $(RELEASE_BIN_FILE) 0x8000000" -c "verify_image $(RELEASE_BIN_FILE) 0x8000000" -c "reset run" -c shutdown
-	openocd -f interface/stlink-v2-1.cfg -f $(OPENOCD_FILE) -c "reset init" -c "reset halt" -c halt -c "flash write_image erase $(RELEASE_BIN_FILE) 0x8000000" -c "verify_image $(RELEASE_BIN_FILE) 0x8000000" -c "reset run" -c shutdown
+	ifeq ($(OPENOCD_RUNNING), true)
+		$(warn "OpenOCD already running!")
+	endif
+	openocd -f interface/stlink-v2-1.cfg -f $(OPENOCD_FILE) -c init -c "reset halt" -c halt -c "flash write_image erase $(RELEASE_BIN_FILE) 0x8000000" -c "verify_image $(RELEASE_BIN_FILE) 0x8000000" -c "reset run" -c shutdown
 
 load-debug: debug
-#	openocd -f interface/stlink-v2-1.cfg -f $(OPENOCD_FILE) -c init -c "reset halt" -c halt -c "flash write_image erase $(DEBUG_BIN_FILE) 0x8000000" -c "verify_image $(DEBUG_BIN_FILE) 0x8000000" -c "reset run" -c shutdown
-	openocd -f interface/stlink-v2-1.cfg -f $(OPENOCD_FILE) -c "reset init" -c "reset halt" -c halt -c "flash write_image erase $(DEBUG_BIN_FILE) 0x8000000" -c "verify_image $(DEBUG_BIN_FILE) 0x8000000" -c "reset run" -c shutdown
+	openocd -f interface/stlink-v2-1.cfg -f $(OPENOCD_FILE) -c init -c "reset halt" -c halt -c "flash write_image erase $(DEBUG_BIN_FILE) 0x8000000" -c "verify_image $(DEBUG_BIN_FILE) 0x8000000" -c "reset run" -c shutdown
 
 # Use this if you want gdb to be rtos thread aware
 connect-rtos: load-debug
-#	openocd -f interface/stlink-v2-1.cfg -f $(OPENOCD_FILE) -c "stm32f7x.cpu configure -rtos FreeRTOS" -c init -c "reset halt" -c halt
-	openocd -f interface/stlink-v2-1.cfg -f $(OPENOCD_FILE) -c "stm32f7x.cpu configure -rtos FreeRTOS" -c "reset init" -c "reset halt" -c halt
+	openocd -f interface/stlink-v2-1.cfg -f $(OPENOCD_FILE) -c "stm32f7x.cpu configure -rtos FreeRTOS" -c init -c "reset halt" -c halt
 
 # use this to debug stuff before rtos starts
 connect: load-debug
+	OPENOCD_RUNNING = true
 	openocd -f interface/stlink-v2-1.cfg -f $(OPENOCD_FILE) -c init -c "reset halt" -c halt
+	OPENOCD_RUNNING = false
 
 #=======
 #load: release
