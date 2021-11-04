567ace4a5b5c6997a7db8afbcf350b1ed32e3d92
123p10 <obrake3@gmail.com>
2021-11-04 11:24:43 -0700

    Add Junk Byte

--- Src/F7_Src/ltc_common.c
+++ Src/F7_Src/ltc_common.c
@@ -139,9 +139,16 @@ int batt_spi_wakeup(bool sleeping)
     if (sleeping) {
         for (int board = 0; board < NUM_BOARDS; board++) {
             HAL_GPIO_WritePin(ISO_SPI_NSS_GPIO_Port, ISO_SPI_NSS_Pin, GPIO_PIN_RESET);
-            delay_us(400);
+            delay_us(300);
             HAL_GPIO_WritePin(ISO_SPI_NSS_GPIO_Port, ISO_SPI_NSS_Pin, GPIO_PIN_SET);
             delay_us(10);
+        }
+        for (int board = 0; board < NUM_BOARDS; board++) {
+            if (batt_spi_tx(&dummy, JUNK_SIZE))
+            {
+                ERROR_PRINT("Failed to wakeup batt spi\n");
+                return 1;
+            }
         }
     } else {
         for (int board = 0; board < NUM_BOARDS; board++) {
