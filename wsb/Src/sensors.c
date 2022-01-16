e44358b2d5cac6e2ea3b39502056dfc223234ea9
123p10 <obrake3@gmail.com>
2022-01-15 20:38:44 -0500

    Add encoder to mm

--- wsb/Src/sensors.c
+++ wsb/Src/sensors.c
@@ -9,25 +9,41 @@
 #include "userCan.h"
 #include "stdbool.h"
 
+// Task Information
 #define POLL_SENSORS_TASK_ID 2
 #define POLL_SENSORS_PERIOD_MS 1000
 
+
+// Encoder Information
 #define ENCODER_COUNTER (__HAL_TIM_GET_COUNTER(&ENCODER_TIM_HANDLE))
+#define ENCODER_PULSES_PER_REVOLUTION (12)
+#define WHEEL_DIAMETER_MM (546)
+
+// About a 0.6% error due to integer rounding of PI
+// Increasing scale of PI does not improve accuracy
+#define PI_SCALE (100)
+#define PI_SCALED (314)
+#define ENCODER_COUNT_TO_MM(count) ((count)*(((WHEEL_DIAMETER_MM*PI_SCALED)/PI_SCALE)/12))
+
 
 typedef struct {
 	volatile uint32_t encoder_counts;
+	volatile uint32_t encoder_mm;
 } sensors_data_S;
 
 
 static sensors_data_S sensors_data;
 
-static void pollEncoder(void)
+static void transmit_sensor_values(void);
+
+static void poll_encoder(void)
 {
 	static uint16_t last_count = 0;
 
 	// TIM3 uses lower 16 bits
 	uint16_t current_count = ENCODER_COUNTER;
 	sensors_data.encoder_counts += ((uint32_t)current_count - (uint32_t)last_count);
+	sensors_data.encoder_mm = ENCODER_COUNT_TO_MM(sensors_data.encoder_mm);
 	last_count = current_count;
 }
 
@@ -44,7 +60,9 @@ void pollSensorsTask(void const * argument)
     while(1)
 	{
 
-		pollEncoder();
+		poll_encoder();
+
+		transmit_sensor_values();
 
         watchdogTaskCheckIn(POLL_SENSORS_TASK_ID);
 		
@@ -54,8 +72,13 @@ void pollSensorsTask(void const * argument)
 	}
 }
 
+static void transmit_sensor_values(void)
+{
+	// Send over CAN
+}
 
 uint32_t sensor_encoder_count(void)
 {
 	return sensors_data.encoder_counts;
 }
+
