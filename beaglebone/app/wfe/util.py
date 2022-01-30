558474a19a6bf1de25f19aef612461b3209c74ee
Zulaikha Zakiullah <zzakiullah1@gmail.com>
2022-01-30 11:06:08 -0500

    Display DTC messages in dashboard

--- beaglebone/app/wfe/util.py
+++ beaglebone/app/wfe/util.py
@@ -2,3 +2,7 @@ import pkg_resources
 
 def default_dbc_path():
     return pkg_resources.resource_filename(__name__, "../../../../common/Data/2018CAR.dbc")
+
+def default_dtc_path():
+    return pkg_resources.resource_filename(__name__, "../../../../common/Data/DTC.csv")
+
