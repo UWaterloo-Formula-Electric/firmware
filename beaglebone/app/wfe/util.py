7a7b9d65939074499d6f74144b8f16f57da81ac1
Owen Brake <obrake3@gmail.com>
2022-02-10 02:24:05 +0000

    Merged in WFE-327/bb-app (pull request #15)
    WFE-327,1358 - Automatically Provision app code with Beaglebone and Add Cronjob
    
    * Add app provisioning to beaglebone
    
    * Add crontab
    
    * Fix DBC paths
    
    * Add CLI argument

--- beaglebone/app/wfe/util.py
+++ beaglebone/app/wfe/util.py
@@ -1,4 +1,4 @@
 import pkg_resources
 
 def default_dbc_path():
-    return pkg_resources.resource_filename(__name__, "../../../../common/Data/2018CAR.dbc")
+    return pkg_resources.resource_filename(__name__, "/home/wfe/data/2018CAR.dbc")
