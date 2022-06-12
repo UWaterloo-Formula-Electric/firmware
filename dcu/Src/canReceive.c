8090b1f92cfb1ed37159a01a2d24e3d477e19375
Jacky Lim <cllim@uwaterloo.ca>
2022-06-11 22:27:28 -0400

    resolve merge conflict

--- dcu/Src/canReceive.c
+++ dcu/Src/canReceive.c
@@ -67,12 +67,10 @@ void CAN_Msg_BMU_DTC_Callback(int DTC_CODE, int DTC_Severity, int DTC_Data)
     {
         ERROR_PRINT_ISR("Got IMD failure\n");
         IMD_FAIL_LED_ON
-    } 
-    else if ((DTC_CODE == CRITICAL_CELL_VOLTAGE_LOW)
+    } else if ((DTC_CODE == CRITICAL_CELL_VOLTAGE_LOW)
                || (DTC_CODE == CRITICAL_CELL_VOLTAGE_HIGH)
                || (DTC_CODE == CRITICAL_CELL_TEMP_HIGH)
-               || (DTC_CODE == CRITICAL_CELL_TEMP_LOW))
-    {
+               || (DTC_CODE == CRITICAL_CELL_TEMP_LOW)) {
         ERROR_PRINT_ISR("Got AMS failure\n");
         AMS_FAIL_LED_ON
     }
