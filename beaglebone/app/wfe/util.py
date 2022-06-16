51b54ea972c8430b2e36111962ca06cc11779192
Justin Vuong <j23vuong@uwaterloo.ca>
2022-06-16 20:10:17 +0000

    Update the default paths

--- beaglebone/app/wfe/util.py
+++ beaglebone/app/wfe/util.py
@@ -1,7 +1,7 @@
 import pkg_resources
 
 def default_dbc_path():
-    return pkg_resources.resource_filename(__name__, "/home/debian/firmware/Data/2018CAR.dbc")
+    return "/home/debian/firmware/Data/2018CAR.dbc"
 
 def default_dtc_path():
-    return pkg_resources.resource_filename(__name__, "/home/debian/firmware/Data/DTC.csv")
+    return "/home/debian/firmware/Data/DTC.csv"
