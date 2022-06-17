c1fceeb38a5e680d399cc313d448cf0f7f3a11e2
Justin Vuong <j23vuong@uwaterloo.ca>
2022-06-17 14:46:07 +0000

    Updated path due to copying error from BB

--- beaglebone/app/wfe/util.py
+++ beaglebone/app/wfe/util.py
@@ -1,7 +1,7 @@
 import pkg_resources
 
 def default_dbc_path():
-    return "/home/debian/firmware/Data/2018CAR.dbc"
+    return "/home/debian/firmware/common/Data/2018CAR.dbc"
 
 def default_dtc_path():
-    return "/home/debian/firmware/Data/DTC.csv"
+    return "/home/debian/firmware/common/Data/DTC.csv"
