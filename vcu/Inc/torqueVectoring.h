e0475ab07eb0e576a5d8c404f03113a3aa9a8e8e
Zulaikha Zakiullah <zzakiullah1@gmail.com>
2022-07-09 16:13:36 -0400

    Add initial files

---
+++ vcu/Inc/torqueVectoring.h
@@ -0,0 +1,9 @@
+#ifndef TORQUEVECTORING_H
+#define TORQUEVECTORING_H
+
+#define TV_FACTOR 2.5
+
+float motorRightTorque(torqueRight, steeringAngle);
+float motorLeftTorque(torqueLeft, steeringAngle);
+
+#endif
---
+++ vcu/Src/torqueVectoring.c
@@ -0,0 +1,11 @@
+#include "torqueVectoring.h"
+
+float motorRightTorque(torqueRight, steeringAngle)
+{
+
+}
+
+float motorLeftTorque(torqueLeft, steeringAngle)
+{
+
+}
