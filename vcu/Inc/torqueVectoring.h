7df5ecd3beeed0a6fbafe2ef53091ccf3cc870d5
Zulaikha Zakiullah <zzakiullah1@gmail.com>
2022-07-09 16:31:36 -0400

    Add to torque vectoring algorithm

--- vcu/Inc/torqueVectoring.h
+++ vcu/Inc/torqueVectoring.h
@@ -1,9 +1,9 @@
 #ifndef TORQUEVECTORING_H
 #define TORQUEVECTORING_H
 
-#define TV_FACTOR 2.5
+#define TV_FACTOR 2.5   // Can be adjusted when testing
 
-float motorRightTorque(torqueRight, steeringAngle);
-float motorLeftTorque(torqueLeft, steeringAngle);
+float motorRightTorque(float torqueRight, float steeringAngle);
+float motorLeftTorque(float torqueLeft, float steeringAngle);
 
 #endif
--- vcu/Src/torqueVectoring.c
+++ vcu/Src/torqueVectoring.c
@@ -1,11 +1,23 @@
 #include "torqueVectoring.h"
 
-float motorRightTorque(torqueRight, steeringAngle)
-{
+/**
+ * The below algorithm assumes that:
+ *      - Positive angle => clockwise rotation (right turn)
+ *      - Negative angle => counterclockwise rotation (left turn)
+ *
+ * Also assumes that 0 degrees => no turn of steering wheel
+ *
+ * NEEDS TO BE TESTED!!
+ */
 
+// Currently does not account if value is negative - will wheel reverse in that case?
+
+float motorRightTorque(float torqueRight, float steeringAngle)
+{
+    return torqueRight + steeringAngle * TV_FACTOR;
 }
 
-float motorLeftTorque(torqueLeft, steeringAngle)
+float motorLeftTorque(float torqueLeft, float steeringAngle)
 {
-
+    return torqueLeft - steeringAngle * TV_FACTOR;
 }
