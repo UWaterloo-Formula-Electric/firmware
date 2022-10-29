8db9df9a80eb4474da7568ff0ef7ef8f45179df2
Justin Vuong <j23vuong@uwaterloo.ca>
2022-10-28 20:27:42 -0400

    Updated Can Monitor Filenames to prevent overlaps

--- beaglebone/app/wfe/can_monitor.sh
+++ beaglebone/app/wfe/can_monitor.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 echo "Started can_monitor script"
-while ! ip link show can1 | grep "UP" > /dev/null; do 
-	:
+while ! ip link show can1 | grep "UP" > /dev/null; do
+        :
 done
 echo "CAN1 is up"
 
 n=0;
 max_attempts=50;
-d=$(date +%b%d_%Y_%k_%M_%S)
-f_name="${d}.log"
+
+i=1
+while true
+do
+        logname="/home/debian/can_monitor_logs/$i.log"
+        if [[ ! -f $logname ]]
+        then
+                break
+        fi
+        i=$((i+1))
+
+done
+
 echo $d
 while [ $n -lt $max_attempts ]; do
-	echo "Attempting candump for the ${n}th time"
-	candump can1 -t a > /home/debian/can_monitor_logs/$f_name
-	sleep 0.25;
-	let n=$n+1
+        echo "Attempting candump for the ${n}th time"
+        candump can1 -t a > $logname
+        sleep 0.25;
+        let n=$n+1
 done
