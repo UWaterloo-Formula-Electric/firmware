031427ccd90e38b8401bee577daf40554cf8fd8c
Jacky Lim <cllim@uwaterloo.ca>
2022-11-08 21:06:47 +0000

    Merged in WFE-2191/publish-speed (pull request #109)
    WFE-2191/publish speed
    
    * Add macro for converting rad/s and RPM to kph
    
    * Undo changes to get_speed functions
    
    - The formulas are now applied where the function is called
    - Define two new macros for calculation
    
    * fix minor error
    
    * Add CAN message for speed in kph
    
    - message sent to beaglebone
    - priority: 2
    
    * Add more macros for clarity
    
    * fix merge conflict
    
    * move publish tasks out of traction control
    
    * increase bit size for signals
    
    * minor fix
    
    * fix offset issue
    
    
    Approved-by: Justin Vuong

--- vcu/Src/traction_control.c
+++ vcu/Src/traction_control.c
@@ -23,10 +23,16 @@ We want to do (((int32_t)rpm) - 32768)  where the driver will do  (int32_t)((uin
 #define TRACTION_CONTROL_TASK_PERIOD_MS 200
 #define RPM_TO_RADS(rpm) ((rpm)*2*PI/60.0f)
 
+// Macros for converting RPM to KPH
+#define GEAR_RATIO 15.0/52.0
+#define M_TO_KM 1.0/1000.0f
+#define WHEEL_DIAMETER_M 0.52
+#define WHEEL_CIRCUMFERENCE WHEEL_DIAMETER_M*PI
+#define HOUR_TO_MIN 60
+#define RPM_TO_KPH(rpm) ((rpm)*HOUR_TO_MIN*WHEEL_CIRCUMFERENCE*M_TO_KM*GEAR_RATIO)
 
 // For every 1rad/s, decrease torque by kP
 #define kP_DEFAULT (0.1f)
-
 
 // With our tire radius, rads/s ~ km/h
 #define ERROR_FLOOR_RADS_DEFAULT (20.0f)
@@ -92,23 +98,32 @@ void tractionControlTask(void *pvParameters)
 	float error_right = 0.0f; //error between right rear and front
 	TickType_t xLastWakeTime = xTaskGetTickCount();
 
+	//initialized variables so that speed is 0 on startup 
+	VCU_wheelSpeed_RR = MC_ENCODER_OFFSET;
+	VCU_wheelSpeed_RL = MC_ENCODER_OFFSET; 
+
 	while(1)
 	{
 		torque_max = MAX_TORQUE_DEMAND_DEFAULT;
 
+		FR_speed = get_FR_speed(); 
+		FL_speed = get_FL_speed(); 
+		RR_speed = get_RR_speed(); 
+		RL_speed = get_RL_speed(); 
+
+		VCU_wheelSpeed_RR = RR_speed;
+		sendCAN_TC_wheelSpeed_right();
+
+		VCU_wheelSpeed_RL = RL_speed;
+		sendCAN_TC_wheelSpeed_left();
+
+		SpeedMotorRightKPH = RPM_TO_KPH(((int64_t)SpeedMotorRight) - MC_ENCODER_OFFSET);
+		SpeedMotorLeftKPH = RPM_TO_KPH(((int64_t)SpeedMotorLeft) - MC_ENCODER_OFFSET);
+		sendCAN_SpeedMotorKPH();
+
 		if(tc_on)
 		{
 			torque_adjustment = 0.0f;
-			FR_speed = get_FR_speed(); 
-			FL_speed = get_FL_speed(); 
-			RR_speed = get_RR_speed(); 
-			RL_speed = get_RL_speed(); 
-
-			VCU_wheelSpeed_RR = RR_speed;
-			sendCAN_TC_wheelSpeed_right();
-
-			VCU_wheelSpeed_RL = RL_speed;
-			sendCAN_TC_wheelSpeed_left();
 
 			error_left = RL_speed - FL_speed;
 			error_right = RR_speed - FR_speed;
