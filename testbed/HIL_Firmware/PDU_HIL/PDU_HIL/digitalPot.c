25d3d9614df708a1f61ef63a5206167eedd62009
Daniel Bishara <d2bishar@uwaterloo.ca>
2023-04-25 16:43:41 -0400

    added function to set pot resistance

--- testbed/HIL_Firmware/PDU_HIL/PDU_HIL/digitalPot.c
+++ testbed/HIL_Firmware/PDU_HIL/PDU_HIL/digitalPot.c
@@ -6,10 +6,38 @@
 #include "driver/spi_master.h"
 #include "driver/gpio.h"
 #include "driver/spi_master.h"
+#include "esp_err.h"
+#include "digitalPot.h"
+#include "userInit.h"
 
 //Digital pot being used https://www.analog.com/media/en/technical-documentation/data-sheets/AD5260_5262.pdf
 
-void setPotResitance (uint8_t resistance)
+int setPotResitance (uint16_t resistance)
 {
-    
+    uint8_t out_value = 0;
+
+    if (resistance > POT_LIMIT)
+    {
+        resistance = POT_LIMIT;
+    }
+
+    out_value = ((resistance - WIPER_RESISTANCE_OHM)/NOMINAL_RESISTANCE_OHM)*POT_LIMIT;
+
+    spi_transaction_t trans = {
+        .tx_data [0] = out_value,
+        .length = 8,
+        .flags = SPI_TRANS_USE_TXDATA,
+    };
+
+    esp_err_t fault = 0;
+
+    fault = spi_device_transmit(pot, &trans);
+    if(fault != ESP_OK)
+    {
+        printf("Failed transmit data\n");
+        return ESP_FAIL;
+    }
+
+    return ESP_OK;
+
 }
--- testbed/HIL_Firmware/PDU_HIL/PDU_HIL/include/digitalPot.h
+++ testbed/HIL_Firmware/PDU_HIL/PDU_HIL/include/digitalPot.h
@@ -1,6 +1,10 @@
 #ifndef DIGITAL_POT_H
 #define DIGITAL_POT_H
 
+#define POT_LIMIT 256
+#define WIPER_RESISTANCE_OHM 60
+#define NOMINAL_RESISTANCE_OHM 200000
 
+int setPotResitance (uint16_t resistance);
 
 #endif
