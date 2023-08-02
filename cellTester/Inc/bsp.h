c271c0cbc2af0404900d7dccf120bbc5cf8b90e8
Justin Vuong <j23vuong@uwaterloo.ca>
2023-08-01 22:47:47 -0400

    Added cellTester Cube project

---
+++ cellTester/Inc/bsp.h
@@ -0,0 +1,38 @@
+#ifndef __BSP_H
+#define __BSP_H
+
+#include "boardTypes.h"
+#include "main.h"
+#include "tim.h"
+#include "usart.h"
+#include "stdbool.h"
+#include "iwdg.h"
+
+// cppcheck-suppress misra-c2012-20.9
+#if IS_BOARD_F0
+#include "stm32f0xx_hal.h"
+
+// TODO: Set these to the right values
+#define DEBUG_UART_HANDLE huart2
+#define IWDG_HANDLE hiwdg
+#define STATS_TIM_HANDLE htim2
+
+#else
+
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+#pragma message "BOARD_TYPE_NUCLEO_F0: " #BOARD_TYPE_NUCLEO_F0
+#error Compiling for unkown board type
+
+#endif
+
+#define DISABLE_CAN_FEATURES
+
+// Comment out to remove debug printing
+#define DEBUG_ON
+
+// Comment out to remove error printing
+#define ERROR_PRINT_ON
+
+#define CONSOLE_PRINT_ON
+#endif /* __BSP_H */
---
+++ cellTester/Src/userInit.c
@@ -0,0 +1,35 @@
+/**
+  *****************************************************************************
+  * @file    userInit.c
+  * @author  Justin Vuong
+  * @brief   Initialization before RTOS starts
+  * @details Contains the userInit function, which is called before the RTOS
+  * starts to allow the user to initialize modules or other things that must be
+  * done before the RTOS starts
+  ******************************************************************************
+  */
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "bsp.h"
+#include "debug.h"
+
+void vApplicationStackOverflowHook( TaskHandle_t xTask,
+                                    signed char *pcTaskName )
+{
+    printf("Stack overflow for task %s\n", pcTaskName);
+}
+
+
+// This is declared with weak linkage in all Cube main.c files, and called
+// before freeRTOS initializes and starts up
+void userInit()
+{
+    /* Should be the first thing initialized, otherwise print will fail */
+    if (debugInit() != HAL_OK) {
+        Error_Handler();
+    }
+
+    uartStartReceiving(&DEBUG_UART_HANDLE);
+}
