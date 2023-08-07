789d7d14c63fe995425800beb27234f9f1f392db
Daniel Bishara <d2bishar@uwaterloo.ca>
2023-08-07 01:10:34 -0400

    ported over code from BMU

---
+++ cellTester/Inc/ade7913.h
@@ -0,0 +1,12 @@
+#ifndef ADE7013_H
+#define ADE7013_H
+
+HAL_StatusTypeDef hvadc_init();
+HAL_StatusTypeDef adc_read(uint8_t addr, int32_t *dataOut);
+HAL_StatusTypeDef adc_readbyte(uint8_t addr, uint8_t *dataOut);
+HAL_StatusTypeDef adc_write(uint8_t addr, uint8_t data);
+HAL_StatusTypeDef adc_read_current(float *dataOut);
+HAL_StatusTypeDef adc_read_v1(float *dataOut);
+HAL_StatusTypeDef adc_read_v2(float *dataOut);
+
+#endif /* ADE7013_H*/
