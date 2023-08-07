13a5646c7e53391961f5ea98e23117399da005b7
Justin Vuong <j23vuong@uwaterloo.ca>
2023-08-07 14:22:56 -0400

    Add all interfaces and fix linking errors

--- cellTester/Cube-F0-Src/cellTester/Src/gpio.c
+++ cellTester/Cube-F0-Src/cellTester/Src/gpio.c
@@ -42,9 +42,23 @@
 void MX_GPIO_Init(void)
 {
 
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(ISO_ADC_CS_GPIO_Port, ISO_ADC_CS_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : PtPin */
+  GPIO_InitStruct.Pin = ISO_ADC_CS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(ISO_ADC_CS_GPIO_Port, &GPIO_InitStruct);
 
 }
 
