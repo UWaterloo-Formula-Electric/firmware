8dd8081c4cb16d7241128e4bc87880a51e19d38f
Justin Vuong <j23vuong@uwaterloo.ca>
2023-08-14 17:56:13 -0400

    Firmware integration working
    Validated Code

--- cellTester/Src/mainTaskEntry.c
+++ cellTester/Src/mainTaskEntry.c
@@ -19,7 +19,7 @@
 #include "ade7913.h"
 #include "temperature.h"
 
-#define MAIN_TASK_PERIOD 1000
+#define MAIN_TASK_PERIOD 3
 #define CELL_STABILIZATION_TIME_MS 10
 
 void printCellValues(void);
@@ -57,43 +57,28 @@ void mainTaskFunction(void const* argument) {
     // 3. Wait for cell to stabilize
     // 4. Take measurement
     // 5. Repeat 2-4 until cell current is at max
-    set_PWM_Duty_Cycle(&FET_TIM_HANDLE, 0);
+    set_PWM_Duty_Cycle(&FET_TIM_HANDLE, 0.0f);
     DEBUG_PRINT("time (ms), isCharacterising, PWM duty (%%), v1 (V), v2 (V), Ishunt (A), Temp1 (C), Temp2 (C)\n");
 
     while (1) {
-        if (isCharacterizationRunning) {
-            for (float dutyCycle = 0; dutyCycle <= 100; dutyCycle += 0.5) {
-                if (!isCharacterizationRunning)
-                    break;
-                set_PWM_Duty_Cycle(&FET_TIM_HANDLE, dutyCycle);
-                vTaskDelay(pdMS_TO_TICKS(CELL_STABILIZATION_TIME_MS));
-                printCellValues();
-                vTaskDelay(pdMS_TO_TICKS(10));
-            }
-            isCharacterizationRunning = false;
-            set_PWM_Duty_Cycle(&FET_TIM_HANDLE, 0);
-        }
         printCellValues();
-        printThermistorValues();
+        // printThermistorValues();
         vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(MAIN_TASK_PERIOD));
     }
 }
 
 void printCellValues(void) {
-    float v1 = 0.0f;
-    float v2 = 0.0f;
+    // float v1 = 0.0f;
+    // float v2 = 0.0f;
     float I = 0.0f;
-    adc_read_v1(&v1);
-    adc_read_v2(&v2);
+    // adc_read_v1(&v1);
+    // adc_read_v2(&v2);
     adc_read_current(&I);
     // Timestamp, Charecterization Enabled, Voltage, Current, Temperature
-    DEBUG_PRINT("%lu, %u, %.3lf, %.3lf, %.3lf, %.2lf, ",
-                HAL_GetTick()/1000,
-                isCharacterizationRunning,
+    DEBUG_PRINT("%lu, %.3lf, %.3lf\n",
+                HAL_GetTick(),
                 get_PWM_Duty_Cycle(),
-                v1,
-                I,
-                v2);
+                I);
 }
 
 void printThermistorValues(void)
