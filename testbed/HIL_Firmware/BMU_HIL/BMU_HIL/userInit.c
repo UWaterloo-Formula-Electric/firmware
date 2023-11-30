811b4d88cf1e549ef11b857a23983d6ec6e4a0e9
Behrad Assa <behradassa@gmail.com>
2023-11-29 19:32:46 -0500

    Created driver initiailization for BMU_HIL

--- testbed/HIL_Firmware/BMU_HIL/BMU_HIL/userInit.c
+++ testbed/HIL_Firmware/BMU_HIL/BMU_HIL/userInit.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include "driver/gpio.h"
+#include "driver/twai.h"
 
-void app_main(void)
+void app_main()
 {
+    //Initialize configuration structures using macro initializers
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_21, GPIO_NUM_22, TWAI_MODE_NORMAL);
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
+    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+
+    //Install TWAI driver
+    if (twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
+        printf("Driver installed\n");
+    } else {
+        printf("Failed to install driver\n");
+        return;
+    }
+
+    //Start TWAI driver
+    if (twai_start() == ESP_OK) {
+        printf("Driver started\n");
+    } else {
+        printf("Failed to start driver\n");
+        return;
+    }
+
+    ...
 
 }
