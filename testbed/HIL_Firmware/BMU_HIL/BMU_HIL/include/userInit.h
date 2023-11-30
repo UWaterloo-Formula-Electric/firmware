20d9b5f42408775cb55129a0af1dca765b2b6c9a
Behrad Assa <behradassa@gmail.com>
2023-11-29 19:44:38 -0500

    Configured GPIO pins for CAN_TX, CAN_RX

--- testbed/HIL_Firmware/BMU_HIL/BMU_HIL/include/userInit.h
+++ testbed/HIL_Firmware/BMU_HIL/BMU_HIL/include/userInit.h
@@ -1,8 +1,12 @@
 #ifndef USER_INIT_H
 #define USER_INIT_H
 #include "driver/spi_master.h"
-// #define CAN_TX GPIO_NUM_12
-// #define CAN_RX GPIO_NUM_13
+
+
+#define CAN_TX GPIO_NUM_36
+#define CAN_RX GPIO_NUM_35
+
+
 void taskRegister (void);
 esp_err_t CAN_init (void);
 esp_err_t spi_init(void);
