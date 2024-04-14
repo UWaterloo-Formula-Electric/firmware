e7f689d55fbd25c00810a0c4372c4afecc374412
Alex Dubljevic <Alex.dubljevic@gmail.com>
2024-04-14 17:52:42 -0400

    added cope test file lol

---
+++ beaglebone/app/wfe/testing.py
@@ -0,0 +1,10 @@
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, Scale, scrolledtext
+
+window = Tk()
+
+window.geometry("800x480")
+window.configure(bg = "#FFFFFF")
+window.title("UWFE Dashboard (LIGHT)")
+
+window.resizable(False, False)
+window.mainloop()
