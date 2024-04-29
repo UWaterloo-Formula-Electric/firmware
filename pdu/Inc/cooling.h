705d6562dbbc8cb2cacbfb4ef830a317370cac7b
Addison <74084822+addison-ch@users.noreply.github.com>
2024-04-29 01:52:26 -0400

    add conditional cooling

--- pdu/Inc/cooling.h
+++ pdu/Inc/cooling.h
@@ -4,9 +4,9 @@
 
 #define COOLING_TASK_PERIOD_MS 10000 // 5 seconds
 #define COOLING_INCREMENT_MS 100 // 100 ms
-#define COOLING_AMBIENT_TEMP_C 10.0f // Todo - figure out how to acc get ambient
-#define COOLING_MOTOR_MAX_TEMP_C 120.0f
-#define COOLING_INV_MAX_TEMP_C 120.0f
+#define COOLING_AMBIENT_TEMP_C 15.0f // Todo - figure out how to acc get ambient
+#define COOLING_MOTOR_MAX_TEMP_C 90.0f
+#define COOLING_INV_MAX_TEMP_C 90.0f
 #define MC_MESSAGE_LOST_THRESHOLD_MS 200 
 
 typedef enum Cooling_States_t {
--- pdu/Src/cooling.c
+++ pdu/Src/cooling.c
@@ -14,7 +14,6 @@
 #include "LTC4110.h"
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define SCALE_TEMP_PERCENT(temp, max_temp) (((temp) - COOLING_AMBIENT_TEMP_C) / ((max_temp) - COOLING_AMBIENT_TEMP_C))
 
 void coolingOff(void) {
     DEBUG_PRINT("Turning cooling off\n");
@@ -32,6 +31,12 @@ void coolingOn(void) {
     FAN_RIGHT_ENABLE;
 }
 
+int get_duration_from_temp(float temp, float max_temp) {
+    float interpolated_percent = (((temp) - COOLING_AMBIENT_TEMP_C) / ((max_temp) - COOLING_AMBIENT_TEMP_C));
+    int cool_duration_ms = interpolated_percent * COOLING_TASK_PERIOD_MS;
+    return cool_duration_ms;
+}
+
 void coolingTask(void *pvParameters) {
     if (registerTaskToWatch(COOLING_TASK_ID, 2*COOLING_TASK_PERIOD_MS, false, NULL) != HAL_OK)
     {
@@ -43,10 +48,21 @@ void coolingTask(void *pvParameters) {
 
     while(1)
     {
-        // Only cool if motor controller on and EM enabled
-        // Should we check if MC is on?
         if (fsmGetState(&mainFsmHandle) == STATE_Motors_On) {
-            coolingOn();
+            float avg_inv_module_temp = (INV_Module_A_Temp + INV_Module_B_Temp + INV_Module_C_Temp) / 3;
+            int motor_duration_ms = get_duration_from_temp(INV_Motor_Temp, COOLING_MOTOR_MAX_TEMP_C);
+            int inv_duration_ms = get_duration_from_temp(avg_inv_module_temp, COOLING_INV_MAX_TEMP_C);
+
+            // take maximum duration
+            int cooling_duration_ms = MAX(motor_duration_ms, inv_duration_ms);
+            if (cooling_duration_ms > COOLING_TASK_PERIOD_MS) cooling_duration_ms = COOLING_TASK_PERIOD_MS;
+
+            if (cooling_duration_ms > 0) {
+                DEBUG_PRINT("Toggle cooling for %d ms\n", cooling_duration_ms);
+                coolingOn();
+                vTaskDelay(pdMS_TO_TICKS(cooling_duration_ms));
+                coolingOff();
+            }
         }
         watchdogTaskCheckIn(COOLING_TASK_ID);
         vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(COOLING_TASK_PERIOD_MS));
