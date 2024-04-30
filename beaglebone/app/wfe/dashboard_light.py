9421c612f2d6937047965460d84d297d07a718e3
aryanGajelli <argajelli@gmail.com>
2024-04-30 11:31:46 -0400

    Dash works with vcan

--- beaglebone/app/wfe/dashboard_light.py
+++ beaglebone/app/wfe/dashboard_light.py
@@ -1,55 +1,55 @@
 from threading import Thread
-
+import math
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, Scale, scrolledtext
 import time
 import can
 import cantools
 import csv
 
-CANBUS = 'vcan0'
+CANBUS = 'can1'
+db = cantools.db.load_file('common/Data/2024CAR.dbc')
+can_bus = can.interface.Bus(channel=CANBUS, bustype='socketcan')
 
 # CAN arbitration ID constants
-BATTERYSTATUSHV_ARB_ID = 2281967617
-BMU_DTC_ARB_ID = 2147548929
-MC_TEMP_ARB_ID = 2365563393
-MC_TEMP_INV_ARB_ID = 2365562881
+BATTERYSTATUSHV_ARB_ID = db.get_message_by_name('BMU_batteryStatusHV').frame_id
+BMU_DTC_ARB_ID = db.get_message_by_name('BMU_DTC').frame_id
+MC_TEMP_ARB_ID = db.get_message_by_name('MC_Temperature_Set_3').frame_id
+MC_TEMP_INV_ARB_ID = db.get_message_by_name('MC_Temperature_Set_1').frame_id
 
-TEMPCOOLANT_L_ARB_ID = 2550137362
-TEMPCOOLANT_R_ARB_ID = 2550137363
+TEMPCOOLANT_L_ARB_ID = db.get_message_by_name('TempCoolantLeft').frame_id
+TEMPCOOLANT_R_ARB_ID = db.get_message_by_name('TempCoolantRight').frame_id
 
 # declare water temp variable here because left/right
 # sensor data is from diff CAN messages
 water_temp_left = 0
 water_temp_right = 0
 
-BMU_VBATT_ARB_ID = 2281769985
-WHEELSPEED_ARB_ID = 2287471618
-
-
+BMU_VBATT_ARB_ID = db.get_message_by_name('BMU_AmsVBatt').frame_id
+WHEELSPEED_ARB_ID = db.get_message_by_name('WheelSpeedKPH').frame_id
 
 window = Tk()
 
 window.geometry("800x480")
-window.configure(bg = "#262626")
+window.configure(bg="#262626")
 window.title("UWFE Dashboard (LIGHT)")
 
 canvas = Canvas(
     window,
-    bg = "#262626",
-    height = 480,
-    width = 800,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
+    bg="#262626",
+    height=480,
+    width=800,
+    bd=0,
+    highlightthickness=0,
+    relief="ridge"
 )
 
-canvas.place(x = 0, y = 0)
+canvas.place(x=0, y=0)
 
 charge_bar = canvas.create_rectangle(
     1.0,
     2.0,
     801.0,
-    58.00000000000006,
+    58.0,
     fill="#FF0000",
     outline="")
 
@@ -67,10 +67,11 @@ soc_text = canvas.create_text(
     336.0,
     53.0,
     anchor="nw",
-    text="100%",
+    text="N/A",
     fill="#FFFFFF",
     font=("Lato Bold", 80 * -1)
 )
+
 
 def calculate_charge_color(charge):
     # Calculate a gradient from blue to red based on charge percentage
@@ -81,12 +82,14 @@ def calculate_charge_color(charge):
     color = "#{:02X}{:02X}{:02X}".format(r, g, b)
     return color
 
+
 def update_battery_charge(value):
+
     # Convert value to an integer
     value = int(value)
 
     # Update the text
-    canvas.itemconfig(soc_text, text=f"{value}%")
+    canvas.itemconfig(soc_text, text='%.4s' % ('%.2f' % value) + '%')
 
     # Calculate the color based on the charge percentage
     charge_color = calculate_charge_color(value)
@@ -114,35 +117,23 @@ battery_charge_slider.place(x=370, y=300)  # Adjust the position as needed
 update_battery_charge(battery_charge_slider.get())
 
 
-temp_bg1 = canvas.create_rectangle(
-    0.0,
-    58.0,
-    196.0,
-    150.0,
+temp_batt_bg = canvas.create_rectangle(
+    0.0, 58.0, 196.0, 150.0,
     fill="#7125BD",
     outline="")
 
-temp_bg2 = canvas.create_rectangle(
-    0.0,
-    323.0,
-    196.0,
-    415.0,
+temp_water_bg = canvas.create_rectangle(
+    0.0, 323.0, 196.0, 415.0,
     fill="#0C15EA",
     outline="")
 
-temp_bg3 = canvas.create_rectangle(
-    0.0,
-    234.0,
-    196.0,
-    326.0,
+temp_inv_bg = canvas.create_rectangle(
+    0.0, 234.0, 196.0, 326.0,
     fill="#BA007B",
     outline="")
 
-temp_bg4 = canvas.create_rectangle(
-    0.0,
-    147.0,
-    196.0,
-    239.0,
+temp_motor_bg = canvas.create_rectangle(
+    0.0, 147.0, 196.0, 239.0,
     fill="#FF0101",
     outline="")
 
@@ -186,36 +177,36 @@ battery_temp_text = canvas.create_text(
     6.0,
     76.0,
     anchor="nw",
-    text="D67.3°C",
+    text="N/A",
     fill="#FFFFFF",
-    font=("Lato Bold", 60 * -1)
+    font=("Lato Bold", 50 * -1)
 )
 
 motor_temp_text = canvas.create_text(
     7.0,
     163.0,
     anchor="nw",
-    text="D85.8°C",
+    text="N/A",
     fill="#FFFFFF",
-    font=("Lato Bold", 60 * -1)
+    font=("Lato Bold", 50 * -1)
 )
 
 inverter_temp_text = canvas.create_text(
     4.0,
     253.0,
     anchor="nw",
-    text="D79.9°C",
+    text="N/A",
     fill="#FFFFFF",
-    font=("Lato Bold", 60 * -1)
+    font=("Lato Bold", 50 * -1)
 )
 
 water_temp_text = canvas.create_text(
     4.0,
     340.0,
     anchor="nw",
-    text="D31.6°C",
+    text="N/A",
     fill="#FFFFFF",
-    font=("Lato Bold", 60 * -1)
+    font=("Lato Bold", 50 * -1)
 )
 
 deployment_label = canvas.create_text(
@@ -231,7 +222,7 @@ deployment_text = canvas.create_text(
     384.0,
     134.0,
     anchor="nw",
-    text="D4.8%",
+    text="N/A",
     fill="#FFFFFF",
     font=("Lato Regular", 50 * -1)
 )
@@ -249,7 +240,7 @@ speed_text = canvas.create_text(
     393.0,
     179.0,
     anchor="nw",
-    text="34",
+    text="N/A",
     fill="#FFFFFF",
     font=("Lato Regular", 50 * -1)
 )
@@ -324,19 +315,21 @@ min_cell_label = canvas.create_text(
 )
 
 vbatt_text = canvas.create_text(
-    651.0,
+    620.0,
     77.0,
     anchor="nw",
-    text="D300V",
+    text="N/A",
     fill="#FFFFFF",
     font=("Lato Bold", 40 * -1)
 )
 
+
 def changeMode():
-    if(canvas.itemcget(mode_text, "text") == "RACE"):
+    if (canvas.itemcget(mode_text, "text") == "RACE"):
         canvas.itemconfig(mode_text, text="SLOW")
-    elif(canvas.itemcget(mode_text, "text") == "SLOW"):
+    elif (canvas.itemcget(mode_text, "text") == "SLOW"):
         canvas.itemconfig(mode_text, text="RACE")
+
 
 mode_button = Button(
     window,
@@ -364,7 +357,7 @@ lv_batt_text = canvas.create_text(
     621.0,
     212.0,
     anchor="nw",
-    text="D3.3V",
+    text="N/A",
     fill="#FFFFFF",
     font=("Lato Bold", 40 * -1)
 )
@@ -373,12 +366,12 @@ min_cell_text = canvas.create_text(
     621.0,
     278.0,
     anchor="nw",
-    text="299V",
+    text="N/A",
     fill="#FFFFFF",
     font=("Lato Bold", 40 * -1)
 )
 
-dtc_descriptions = {} 
+dtc_descriptions = {}
 
 with open('common/Data/DTC.csv', 'r') as file:
     reader = csv.reader(file)
@@ -388,36 +381,38 @@ with open('common/Data/DTC.csv', 'r') as file:
         description = row[6]
         dtc_descriptions[dtc_code] = description
 
-db = cantools.database.load_file('common/Data/2024CAR.dbc')
-can_bus = can.interface.Bus(channel=CANBUS, bustype='socketcan')
 
- # Create the scrollable text area
+# Create the scrollable text area
 dtc_text_area = scrolledtext.ScrolledText(window, width=45, height=2)
 dtc_text_area.place(x=10, y=418)
 dtc_text_area.config(font=("Helvetica", 20))
 dtc_text_area.config(background='black')
 dtc_text_area.config(foreground='red')
 
-    # Function to update the text area with new error codes
+# Function to update the text area with new error codes
+
+
 def publish_dtc(error_code, error_message):
     description = dtc_descriptions.get(error_code, "Description not found")
-    dtc_text_area.insert("end","\n" +str(error_code) + " | " + str(description))
-    dtc_text_area.yview("end")  # Scroll to the bottom to show the latest message
+    dtc_text_area.insert("end", "\n" + str(error_code) +
+                         " | " + str(description))
+    # Scroll to the bottom to show the latest message
+    dtc_text_area.yview("end")
+
 
 def openDebug():
     window2 = Tk()
     window2.geometry("800x480")
-    window2.configure(bg = "#262626")
+    window2.configure(bg="#262626")
     window2.title("UWFE Dashboard Debug")
 
     # Reset dashboard button
     reset_button = Button(window2, text="Reset Dashboard")
     reset_button.place(x=10, y=10)
 
-    
-
     # Close Debug Menu button
-    close_button = Button(window2, text="Close Debug Menu",command=window2.withdraw)
+    close_button = Button(window2, text="Close Debug Menu",
+                          command=window2.withdraw)
     close_button.place(x=220, y=10)
 
     # Create the scrollable text area
@@ -426,8 +421,10 @@ def openDebug():
 
     # Function to update the text area with new error codes
     def update_debug_text(error_code):
-        debug_text_area.insert("end", error_code + "                                                " + time.strftime("%H:%M:%S") + "\n")
-        debug_text_area.yview("end")  # Scroll to the bottom to show the latest message  
+        debug_text_area.insert(
+            "end", error_code + "                                                " + time.strftime("%H:%M:%S") + "\n")
+        # Scroll to the bottom to show the latest message
+        debug_text_area.yview("end")
 
     # Function to simulate the stream of diagnostic codes (in a separate thread)
     def simulate_error_stream():
@@ -449,6 +446,7 @@ def openDebug():
     window2.resizable(False, False)
     window2.mainloop()
 
+
 debug_button = Button(
     window,
     text="Debug",
@@ -460,123 +458,116 @@ debug_button = Button(
 
 debug_button.place(x=710, y=440)
 
-OFFSET = 0x8000000
+
 def process_can_messages():
     print("reading can messages...")
     while True:
         message = can_bus.recv(timeout=0.1)
+        if message is None:
+            continue
         print(message)
 
-        if message is not None:
-            
-            if message.arbitration_id > OFFSET:
-                message.arbitration_id -= OFFSET
-            print(message.arbitration_id)
-        # Case for battery temp/soc
-        if message is not None and message.arbitration_id == BATTERYSTATUSHV_ARB_ID:
-            print("message found!")
-            try:
-                decoded_data = db.decode_message(message.arbitration_id, message.data)
+        try:
+            # Case for battery temp/soc
+            if message.arbitration_id == BATTERYSTATUSHV_ARB_ID:
+                decoded_data = db.decode_message(
+                    message.arbitration_id, message.data)
 
                 soc_value = decoded_data['StateBatteryChargeHV']
-                battery_temp = decoded_data['TempCellMax'] 
+                battery_temp = decoded_data['TempCellMax']
 
                 update_battery_charge(soc_value)
-                canvas.itemconfig(battery_temp_text, text=str(battery_temp))
-            except KeyError:
-                print("Unknown message ID")
+                canvas.itemconfig(battery_temp_text, text='%.4s' % ('%.1f' % battery_temp) + '°C')
 
-        # Case for motor temp
-        if message is not None and message.arbitration_id == MC_TEMP_ARB_ID:
-            try:
-                decoded_data = db.decode_message(message.arbitration_id, message.data)
+            # Case for motor temp
+            if message.arbitration_id == MC_TEMP_ARB_ID:
+                decoded_data = db.decode_message(
+                    message.arbitration_id, message.data)
 
                 motor_temp = decoded_data['INV_Motor_Temp']
 
-                canvas.itemconfig(motor_temp_text, text=str(motor_temp))
-            except KeyError:
-                print("Unknown message ID")
+                canvas.itemconfig(motor_temp_text, text='%.4s' % ('%.1f' % motor_temp) + '°C')
 
-        # Case for inverter temp
-        if message is not None and message.arbitration_id == MC_TEMP_INV_ARB_ID:
-            try:
-                decoded_data = db.decode_message(message.arbitration_id, message.data)
+            # Case for inverter temp
+            if message.arbitration_id == MC_TEMP_INV_ARB_ID:
+                decoded_data = db.decode_message(
+                    message.arbitration_id, message.data)
 
                 inv_temp1 = decoded_data['INV_Module_A_Temp']
                 inv_temp2 = decoded_data['INV_Module_B_Temp']
                 inv_temp3 = decoded_data['INV_Module_C_Temp']
 
-                average_inv_temp = (float(inv_temp1) + float(inv_temp2) + float(inv_temp3)) / 3
+                average_inv_temp = (float(inv_temp1) +
+                                    float(inv_temp2) + float(inv_temp3)) / 3
 
-                canvas.itemconfig(inverter_temp_text, text=str(average_inv_temp))
-            except KeyError:
-                print("Unknown message ID")
+                canvas.itemconfig(inverter_temp_text,
+                                  text='%.4s' % ('%.1f' % average_inv_temp) + '°C')
 
-        # Case for water temp (left)
-        if message is not None and message.arbitration_id == TEMPCOOLANT_L_ARB_ID:
-            try:
-                decoded_data = db.decode_message(message.arbitration_id, message.data)
+            # Case for water temp (left)
+            if message.arbitration_id == TEMPCOOLANT_L_ARB_ID:
+                decoded_data = db.decode_message(
+                    message.arbitration_id, message.data)
 
                 water_temp_left = decoded_data['TempInletRadMotorLeft']
-                
-                average_water_temp = (float(water_temp_left) + float(water_temp_right)) / 2
 
-                canvas.itemconfig(water_temp_text, text=str(average_water_temp))
-            except KeyError:
-                print("Unknown message ID")
-        
-        # Case for water temp (right)
-        if message is not None and message.arbitration_id == TEMPCOOLANT_R_ARB_ID:
-            try:
-                decoded_data = db.decode_message(message.arbitration_id, message.data)
+                average_water_temp = (
+                    float(water_temp_left) + float(water_temp_right)) / 2
+
+                canvas.itemconfig(
+                    water_temp_text, text='%.4s' % ('%.1f' % average_water_temp) + '°C')
+
+            # Case for water temp (right)
+            if message.arbitration_id == TEMPCOOLANT_R_ARB_ID:
+
+                decoded_data = db.decode_message(
+                    message.arbitration_id, message.data)
 
                 water_temp_right = decoded_data['TempInletRadMotorRight']
-                
-                average_water_temp = (float(water_temp_left) + float(water_temp_right)) / 2
 
-                canvas.itemconfig(water_temp_text, text=str(average_water_temp))
-            except KeyError:
-                print("Unknown message ID")
+                average_water_temp = (
+                    float(water_temp_left) + float(water_temp_right)) / 2
 
-        # Case for VBATT
-        if message is not None and message.arbitration_id == BMU_VBATT_ARB_ID:
-            try:
-                decoded_data = db.decode_message(message.arbitration_id, message.data)
+                canvas.itemconfig(
+                    water_temp_text, text='%.4s' % ('%.1f' % average_water_temp) + '°C')
+
+            # Case for VBATT
+            if message.arbitration_id == BMU_VBATT_ARB_ID:
+
+                decoded_data = db.decode_message(
+                    message.arbitration_id, message.data)
 
                 vbatt = decoded_data['AMS_PackVoltage']
 
-                canvas.itemconfig(vbatt_text, text=str(vbatt))
-            except KeyError:
-                print("Unknown message ID")
+                canvas.itemconfig(vbatt_text, text= '%.5s'%('%.3f' % vbatt) + 'V')
 
-        # Case for Speeeeeed
-        if message is not None and message.arbitration_id == WHEELSPEED_ARB_ID:
-            try:
-                decoded_data = db.decode_message(message.arbitration_id, message.data)
+            # Case for Speeeeeed
+            if message.arbitration_id == WHEELSPEED_ARB_ID:
+                decoded_data = db.decode_message(
+                    message.arbitration_id, message.data)
 
                 fl_speed = decoded_data['FLSpeedKPH']
                 fr_speed = decoded_data['FRSpeedKPH']
                 rr_speed = decoded_data['RRSpeedKPH']
                 rl_speed = decoded_data['RLSpeedKPH']
-                
-                average_speed = (float(fl_speed) + float(fr_speed) + float(rr_speed) + float(rl_speed)) / 4
 
-                canvas.itemconfig(speed_text, text=str(average_speed))
-            except KeyError:
-                print("Unknown message ID")
+                average_speed = (float(fl_speed) + float(fr_speed) +
+                                 float(rr_speed) + float(rl_speed)) / 4
 
-        # Case for BMU DTC
-        if message is not None and message.arbitration_id == BMU_DTC_ARB_ID:
-            try:
-                decoded_data = db.decode_message(message.arbitration_id, message.data)
+                canvas.itemconfig(speed_text, text='%.3s' % ('%.1f' % battery_temp) + 'kph')
+
+            # Case for BMU DTC
+            if message.arbitration_id == BMU_DTC_ARB_ID:
+                decoded_data = db.decode_message(
+                    message.arbitration_id, message.data)
 
                 DTC_CODE = decoded_data['DTC_CODE']
-                DTC_message = decoded_data['DTC_Data']   
+                DTC_message = decoded_data['DTC_Data']
 
                 publish_dtc(DTC_CODE, DTC_message)
-            except KeyError:
-                print("Unknown message ID")
-        
+        except KeyError:
+            print(
+                f"Message decode failed for {db.get_message_by_frame_id(message.arbitration_id).name}")
+
 
 can_thread = Thread(target=process_can_messages)
 can_thread.start()
