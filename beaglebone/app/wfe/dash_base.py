1cf0f866881173130159da25e0217d8b17439b71
aryanGajelli <argajelli@gmail.com>
2024-05-16 21:11:14 -0700

    dash_base: mvp white bg

---
+++ beaglebone/app/wfe/dash_base.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.wm_geometry("800x480")
+    root.configure(bg="#ffffff")
+    root.title("UWFE Dashboard (LIGHT)")
+    root.mainloop()
