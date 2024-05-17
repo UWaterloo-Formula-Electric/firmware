0619483a7be562ea22ca41e3f167f193d3fd8ce3
aryanGajelli <argajelli@gmail.com>
2024-05-16 21:18:18 -0700

    dash_base: Mainview

--- beaglebone/app/wfe/dash_base.py
+++ beaglebone/app/wfe/dash_base.py
@@ -1,8 +1,19 @@
 import tkinter as tk
 
+class MainView(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+
+        tk.Label(container, text="MainView", font=("Arial", 24)).place(relx=0.5, rely=0.5, anchor="center")
+
 if __name__ == "__main__":
     root = tk.Tk()
+    main = MainView(root)
+    main.place(x=0, y=0, relwidth=1, relheight=1)
     root.wm_geometry("800x480")
-    root.configure(bg="#ffffff")
+    root.configure(bg="#3f3f3f")
     root.title("UWFE Dashboard (LIGHT)")
     root.mainloop()
