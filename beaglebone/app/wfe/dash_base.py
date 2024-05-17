887cd0284a8eef50247ac13401b22d22f3afe45c
aryanGajelli <argajelli@gmail.com>
2024-05-16 21:52:19 -0700

    dash_base: thread that switches between the pages

--- beaglebone/app/wfe/dash_base.py
+++ beaglebone/app/wfe/dash_base.py
@@ -1,22 +1,25 @@
 import tkinter as tk
+import time
+from threading import Thread
+
 
 class Page(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
 
-    def bring_to_front(self):
+    def show(self):
         self.lift()
 
 
 class DashPage(Page):
     def __init__(self, *args, **kwargs):
         Page.__init__(self, *args, **kwargs)
-        tk.Label(self, text="Page 1", font=("Helvetica", 16), background="#ff3f30").place(relx=0.5, rely=0.5, anchor="center")
+        tk.Label(self, text="Page 1", font=("Helvetica", 16), background="#ff3f30").pack(side="top", fill="both", expand=True)
 
 class DebugPage(Page):
     def __init__(self, *args, **kwargs):
         Page.__init__(self, *args, **kwargs)
-        tk.Label(self, text="Page 2", font=("Helvetica", 16), fg="#ffffff", background="#3f3fff").place(relx=0.5, rely=0.5, anchor="center")
+        tk.Label(self, text="Page 2", font=("Helvetica", 16), fg="#ffffff", background="#3f3fff").pack(side="top", fill="both", expand=True)
 
 class MainView(tk.Frame):
     def __init__(self, *args, **kwargs):
@@ -32,7 +35,18 @@ class MainView(tk.Frame):
         self.dashPage.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         self.debugPage.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
-        self.debugPage.bring_to_front()
+
+
+
+def switch_thread():
+    while True:
+        time.sleep(1)
+        print("Switching to DebugPage")
+        main.debugPage.show()
+        time.sleep(1)
+        print("Switching to DashPage")
+        main.dashPage.show()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -41,4 +55,5 @@ if __name__ == "__main__":
     root.wm_geometry("800x480")
     root.configure(bg="#3f3f3f")
     root.title("UWFE Dashboard (LIGHT)")
+    Thread(target=switch_thread, daemon=True).start()
     root.mainloop()
