ad4828bc65dad693c06bd8811a64a1246f19364e
aryanGajelli <argajelli@gmail.com>
2024-05-16 21:19:47 -0700

    dash_base: added page obj and page 1 (only mainview visible)

--- beaglebone/app/wfe/dash_base.py
+++ beaglebone/app/wfe/dash_base.py
@@ -1,13 +1,30 @@
 import tkinter as tk
 
-class MainView(tk.Frame):
+class Page(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
 
+    def bring_to_front(self):
+        self.lift()
+
+
+class DashPage(Page):
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        tk.Label(self, text="Page 1", font=("Helvetica", 16), background="#ff0000").place(relx=0.5, rely=0.5, anchor="center")
+
+
+class MainView(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        self.dashPage = DashPage(self)
+
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
-
+        
         tk.Label(container, text="MainView", font=("Arial", 24)).place(relx=0.5, rely=0.5, anchor="center")
+
+        self.dashPage.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
 if __name__ == "__main__":
     root = tk.Tk()
