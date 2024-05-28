f9ca4f4f793d0307036a111c5e3c35e377f590ea
aryanGajelli <argajelli@gmail.com>
2024-05-27 23:00:00 -0700

    Simulate all possible dtcs on car

--- beaglebone/app/wfe/can_test.py
+++ beaglebone/app/wfe/can_test.py
@@ -1,31 +1,63 @@
 
-import os
+import csv
+import getpass
 from pathlib import Path
+import random
 import can
 import cantools
+from pprint import pprint
 
-if os.getlogin() == 'vagrant':
-    CANBUS = 'vcan0'
+if getpass.getuser() == 'vagrant':
     home_dir = Path("/home/vagrant/shared")
+    CANBUS = 'vcan0'
 else:
-    CANBUS = 'can1'
-    home_dir = Path("/home/debian")
+    home_dir = Path(__file__).resolve().parents[4]
 db = cantools.db.load_file(home_dir / 'firmware/common/Data/2024CAR.dbc')
 can_bus = can.interface.Bus(channel=CANBUS, bustype='socketcan')
 
-db_msg = db.get_message_by_name('BMU_batteryStatusHV')
-ARB_ID = db_msg.frame_id
-data = {}
-for signal in db_msg.signals:
-    data[signal.name] = 0
+dtcs = {}
+with open(home_dir / 'firmware/common/Data/DTC.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip the header row
+            for row in reader:
+                dtc_code = int(row[0])
+                origins = row[2].split(',')
+                for origin in origins:
+                    origin = origin.strip() + "_DTC"
+                    if origin in ("VCU_BEAGLEBONE_DTC", "WSB_DTC"):
+                         continue
+                    dtcs[(dtc_code, origin)] = {
+                        'name': row[1],
+                        'origin': row[2],
+                        'severity': row[3],
+                        'subscribers': row[4],
+                        'data': row[5],
+                        'description': row[6],
+                    }
+for k, v in dtcs.items():
+    dtc_code, dtc_name = k
+    db_msg = db.get_message_by_name(dtc_name)
+    ARB_ID = db_msg.frame_id
 
-data['StateBatteryChargeHV'] = 54.6
-data['TempCellMax'] = 32.5
-# data['ButtonScreenNavLeftEnabled'] = 1
+    desc_split = v['description'].split('{')
+    reasons = {random.randint(0, 255)}
+    if len(desc_split) > 1:
+        reasons = eval("{" + desc_split[1])
+        print(reasons)
+    for reason_data in reasons:
+        data = {
+            'DTC_CODE': dtc_code,
+            'DTC_Severity': int(v['severity']),
+            'DTC_Data': reason_data,
+        }
 
-msg_data = db.encode_message(ARB_ID, data)
+    msg_data = db.encode_message(ARB_ID, data)
 
-msg = can.Message(arbitration_id=ARB_ID, data=msg_data)
+    msg = can.Message(arbitration_id=ARB_ID, data=msg_data)
 
-can_bus.send(msg)
+    can_bus.send(msg)
+
+# use the following to switch between dash pages on vm
+# cansend vcan0 04011007#4000000000000000
+# cansend vcan0 04011007#2000000000000000
 
