6c86a78b186f48c72a7d13f272e44c403fab1690
Kevin Liang <kevinliang22069@gmail.com>
2024-10-04 22:49:26 -0400

    changed timer

--- 2024_wsb/Src/halleffectsensor.c
+++ 2024_wsb/Src/halleffectsensor.c
@@ -19,16 +19,9 @@
 #define HALL_EFFECT_TASK_PERIOD 100
 #define WHEEL_RADIUS 0.40005
 #define PI 3.14156
+#define TICKS_PER_SECOND 1000
 
 /*
- * - hw timer set in hall effect mode, increments each time a gear passes by
- * - reads count diff & time diff
- * IOC FILE CHANGES:
- * - TIM4
- *  - Clock Source: internal clock
- *  - Combined Channels: Hall Sensor Mode
- *  - auto-reload preload: enable
- * - SENSOR_IC_IN: PD12
  *
  * WSBRL CAN message: WSBRL_Speed
  *  signals:
@@ -47,7 +40,7 @@ float getRpm(uint32_t ticks, uint32_t count) {
      * rpm = 60rps
      * 2024 car: 16 teeth
      */
-    return (1.0*count) / (1.0*ticks) / (1.0*NUM_TEETH) * 60.0;
+    return (1.0*count) / (1.0*ticks/TICKS_PER_SECOND) / (1.0*NUM_TEETH) * 60.0;
 }
 
 uint16_t getKph(uint32_t ticks, uint32_t count) { //todo: confirm whether this needs rounding or not
@@ -59,23 +52,48 @@ uint16_t getKph(uint32_t ticks, uint32_t count) { //todo: confirm whether this n
      * kph = 3.6 * m/s
      * 2024 car: effective radius 0.40005m
      */
-    return (uint16_t)((1.0*count) / (1.0*ticks) / (1.0*NUM_TEETH) * (2*PI) * WHEEL_RADIUS * 3.6);
+    return (uint16_t)((1.0*count) / (1.0*ticks/TICKS_PER_SECOND) / (1.0*NUM_TEETH) * (2*PI) * WHEEL_RADIUS * 3.6);
+}
+
+uint32_t pulse_count = 0;
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
+    if (htim->Instance == TIM2) {
+        pulse_count++;
+    }
+}
+
+uint32_t getTickDiff() {
+    static uint32_t prev_tick = 0;
+
+    uint32_t cur_tick = HAL_TIM_ReadCapturedValue(&htim2, TIM_CHANNEL_1);
+
+    uint32_t time_diff;
+    if (cur_tick >= prev_tick) {
+        time_diff = cur_tick-prev_tick;
+    } else {
+        time_diff = (0xFFFFFFFF-prev_tick) + cur_tick + 1;
+    }
+
+    prev_tick = cur_tick;
+
+    return time_diff;
 }
 
 void StartHallEffectSensorTask(void const * argument) {
     DEBUG_PRINT("Starting StartHallEffectSensorTask\n");
     WSBType_t wsbType = detectWSB();
-    if (wsbType != WSBRL || wsbType != WSBRR) {
+    if (wsbType != WSBRL && wsbType != WSBRR) {
         DEBUG_PRINT("Invalid wsb: not WSBRL or WSBRR, deleting StartHallEffectSensorTask\n");
         vTaskDelete(NULL);
         return;
     }
 
-    HAL_TIM_Encoder_Start(&htim4, TIM_CHANNEL_ALL); //todo: move this
+    HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_1); //todo: move this (await Aryan's input)
+
     TickType_t xLastWakeTime = xTaskGetTickCount();
+
     while (1) {
-        uint32_t count = TIM4->CNT;
-        uint32_t ticks = __HAL_TIM_GET_COUNTER(&htim4);
+        uint32_t ticks = getTickDiff();
 
         if (ticks == 0) {
             DEBUG_PRINT("ERROR: ticks = 0\r\n");
@@ -83,11 +101,10 @@ void StartHallEffectSensorTask(void const * argument) {
             continue;
         }
 
-        float rpm = getRpm(ticks, count);
-        uint16_t kph = getKph(ticks, count);
+        float rpm = getRpm(ticks, pulse_count);
+        uint16_t kph = getKph(ticks, pulse_count);
 
-        __HAL_TIM_SET_COUNTER(&htim4, 0);
-        TIM4->CNT = 0;
+        pulse_count = 0;
 
 #if BOARD_ID == ID_WSBRL
         RLSpeedRPM = rpm;
