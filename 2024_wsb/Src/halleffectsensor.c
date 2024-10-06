b5e407e69e11fadf4f4ca5d382aa821451a96b9b
Kevin Liang <kevinliang22069@gmail.com>
2024-10-06 17:55:58 -0400

    committing for pull

--- 2024_wsb/Src/halleffectsensor.c
+++ 2024_wsb/Src/halleffectsensor.c
@@ -21,6 +21,7 @@
 #define WHEEL_RADIUS 0.40005
 #define PI 3.14156
 #define TICKS_PER_SECOND 1000
+#define PERIOD = HALL_EFFECT_TASK_PERIOD/TICKS_PER_SECOND
 
 /*
  *
@@ -35,27 +36,30 @@
  */
 
 
-//todo: cannot use float division
-float getRpm(uint32_t ticks, uint32_t count) {
+float getRps(uint32_t count) {
     /*
      * teeth/s = count diff/time diff
      * rps = 1/num_teeth * teeth/s
+     */
+    return count * (1.0f/PERIOD) * (1.0f/NUM_TEETH);
+}
+
+float getRpm(uint32_t rps) {
+    /*
      * rpm = 60rps
      * 2024 car: 16 teeth
      */
-    return (1.0*count) / (1.0*ticks/TICKS_PER_SECOND) / (1.0*NUM_TEETH) * 60.0;
+    return rps * 60.0;
 }
 
-uint16_t getKph(uint32_t ticks, uint32_t count) { //todo: confirm whether this needs rounding or not
+uint16_t getKph(uint32_t rps) { //todo: confirm whether this needs rounding or not
     /*
-     * teeth/s = count diff/time diff
-     * rps = 1/num_teeth * teeth/s
      * rad/s = 2pi * rps
      * m/s = rad/s * RADIUS
      * kph = 3.6 * m/s
      * 2024 car: effective radius 0.40005m
      */
-    return (uint16_t)((1.0*count) / (1.0*ticks/TICKS_PER_SECOND) / (1.0*NUM_TEETH) * (2*PI) * WHEEL_RADIUS * 3.6);
+    return (uint16_t)(rps * (2*PI) * WHEEL_RADIUS * 3.6);
 }
 
 uint32_t pulse_count = 0;
@@ -63,23 +67,6 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
     if (htim->Instance == TIM1) {
         pulse_count++;
     }
-}
-
-uint32_t getTickDiff() {
-    static uint32_t prev_tick = 0;
-
-    uint32_t cur_tick = HAL_TIM_ReadCapturedValue(&htim1, TIM_CHANNEL_3);
-
-    uint32_t time_diff = 0;
-    if (cur_tick >= prev_tick) {
-        time_diff = cur_tick-prev_tick;
-    } else {
-        time_diff = (0xFFFF-prev_tick) + cur_tick + 1;
-    }
-
-    prev_tick = cur_tick;
-
-    return time_diff;
 }
 
 void HallEffectSensorTask(void const * argument) {
@@ -91,21 +78,13 @@ void HallEffectSensorTask(void const * argument) {
         return;
     }
 
-    HAL_TIM_IC_Start_IT(&htim1, TIM_CHANNEL_3); //todo: move this (await Aryan's input)
-
     TickType_t xLastWakeTime = xTaskGetTickCount();
 
     while (1) {
-        uint32_t ticks = getTickDiff();
 
-        if (ticks == 0) {
-            DEBUG_PRINT("ERROR: ticks = 0\r\n");
-            vTaskDelayUntil(&xLastWakeTime, HALL_EFFECT_TASK_PERIOD);
-            continue;
-        }
-
-        float rpm = getRpm(ticks, pulse_count);
-        uint16_t kph = getKph(ticks, pulse_count);
+        float rps = getRps(pulse_count);
+        float rpm = getRpm(rps);
+        uint16_t kph = getKph(rps);
 
         pulse_count = 0;
 
