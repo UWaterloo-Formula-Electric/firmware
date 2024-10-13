4704ea0631306d63321df79de2d056385a0fc525
Gavin Mesz <gavinator21@gmail.com>
2024-10-13 15:49:36 -0400

    added processCAN.c and header

---
+++ testbed/HIL_Firmware/BMU_HIL/BMU_HIL/src/processCAN.c
@@ -0,0 +1,34 @@
+/***********************************
+************ INCLUDES **************
+************************************/
+//Standard Includes
+#include <stdio.h>
+#include <stdlib.h>
+
+//ESP-IDF Includes
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/twai.h"
+
+//Other Includes
+#include "userInit.h"
+#include "dac.h"
+#include "canReceive.h"
+#include "processCAN.h"
+
+/********************************
+************ Bytes **************
+*********************************/
+static uint16_t byte_1 = 0U;
+static uint16_t byte_2 = 0U;
+
+/***********************************************
+************ Function Definitions **************
+************************************************/
+
+void process_rx_task (void * pvParameters)
+{
+    //TODO: Fill this Task.
+}
+
