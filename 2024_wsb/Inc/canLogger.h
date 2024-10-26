5409973b7b60223a309d61bf402dedef2817bae6
Aryan Gajelli <45264359+aryanGajelli@users.noreply.github.com>
2024-10-26 17:41:41 -0400

    User/agajelli/wsb logging (#52)

---
+++ 2024_wsb/Inc/canLogger.h
@@ -0,0 +1,18 @@
+#ifndef __CANLOGGER_H__
+#define __CANLOGGER_H__
+
+#include <stdbool.h>
+
+#include "FreeRTOS.h"
+#include "stm32f4xx_hal.h"
+#include "stream_buffer.h"
+
+typedef struct {
+    uint32_t id;
+    uint8_t data[8];
+} CanMsg;
+
+extern StreamBufferHandle_t canLogSB;
+extern volatile bool isCanLogEnabled;
+HAL_StatusTypeDef canLogSB_init();
+#endif  // __CANLOGGER_H__
