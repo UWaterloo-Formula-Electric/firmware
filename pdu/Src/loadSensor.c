542d613ccbbe874003339afec9fe8f84e679a995
Rijin <rijin113@gmail.com>
2024-10-31 20:56:54 -0400

    updated code logic for retrieving channel currents

--- pdu/Src/loadSensor.c
+++ pdu/Src/loadSensor.c
@@ -23,26 +23,36 @@
 /*********************************************************************************************************************/
 /*------------------------------------------------------Macros-------------------------------------------------------*/
 /*********************************************************************************************************************/
+#define MAX_LOAD_CHANNELS 14
+#define CHANNEL_BUFFER_LENGTH 1
+#define ENABLE_CHANNEL_FLAG 1
+#define DISABLE_CHANNEL_FLAG 0
+
+// ToDo: Find the correct mapping value
+#define ADC_TO_AMPS_DIVIDER 2.012
 
 /*********************************************************************************************************************/
 /*-------------------------------------------------Global variables--------------------------------------------------*/
 /*********************************************************************************************************************/
-const int enumToEnablePortMap[PDU_NUM_CHANNELS] = {
-    PUMP_1_EN,
-    PUMP_2_EN,
-    CDU_EN,
-    BMU_EN,
-    WSB_EN,
-    TCU_EN,
-    BRAKE_LIGHT_ENABLE,
-    ACC_FANS_EN,
-    INVERTER_EN,
-    RADIATOR_EN,
-    AUX_1_EN,
-    AUX_2_EN,
-    AUX_3_EN,
-    AUX_4_EN
-};
+
+typedef enum PDU_Channels_t {
+    Pump_1_Channel = 1,  // Channel 1
+    Pump_2_Channel,      // Channel 2
+    CDU_Channel,         // Channel 3
+    BMU_Channel,         // Channel 4
+    WSB_1_Channel,       // Channel 5
+    TCU_Channel,         // Channel 6
+    Brake_Light_Channel, // Channel 7
+    Acc_Fans_Channel,    // Channel 8
+    Inverter_Channel,    // Channel 9
+    Radiator_Channel,    // Channel 10
+    Aux_1_Channel,       // Channel 11
+    Aux_2_Channel,       // Channel 12
+    Aux_3_Channel,       // Channel 13
+    Aux_4_Channel,       // Channel 14
+} Load_Channels;
+
+volatile uint32_t adcData[CHANNEL_BUFFER_LENGTH];
 
 /*********************************************************************************************************************/
 /*------------------------------------------------Function Definition------------------------------------------------*/
@@ -52,31 +62,103 @@ const int enumToEnablePortMap[PDU_NUM_CHANNELS] = {
 /*-----------------------------------------------------Helpers-------------------------------------------------------*/
 /*********************************************************************************************************************/
 
-void enableChannel(PDU_Channels_New channel)
+HAL_StatusTypeDef startADCConversions()
 {
-    // Check if valid channel to enable
-    if (channel < 1 || channel > 14)
+    if (HAL_ADC_Start_DMA(&ADC3_HANDLE, (uint32_t*)adcData, MAX_LOAD_CHANNELS) != HAL_OK)
     {
-        // Return Channel Error
+        ERROR_PRINT("Failed to start ADC DMA conversions\n");
+        Error_Handler();
+        return HAL_ERROR;
     }
-    // Enable channel
-    enumToEnablePortMap[channel];
+    if (HAL_TIM_Base_Start(&ADC_TIM_HANDLE) != HAL_OK) {
+        ERROR_PRINT("Failed to start ADC Timer\n");
+        Error_Handler();
+        return HAL_ERROR;
+    }
+    return HAL_OK;
+}
 
-    // Configure the mux based on channel
+void enableDisableChannel(Load_Channels channel, int enableDisableFlag) {
+    if (channel <= MAX_LOAD_CHANNELS) {
+        switch (channel) {
+            case Pump_1_Channel:
+                enableDisableFlag ? PUMP_1_EN : PUMP_1_DISABLE;
+                break;
+            case Pump_2_Channel:
+                enableDisableFlag ? PUMP_2_EN : PUMP_2_DISABLE;
+                break;
+            case CDU_Channel:
+                enableDisableFlag ? CDU_EN : CDU_DISABLE;
+                break;
+            case BMU_Channel:
+                enableDisableFlag ? BMU_EN : BMU_DISABLE;
+                break;
+            case WSB_1_Channel:
+                enableDisableFlag ? WSB_EN : WSB_DISABLE;
+                break;
+            case TCU_Channel:
+                enableDisableFlag ? TCU_EN : TCU_DISABLE;
+                break;
+            case Brake_Light_Channel:
+                enableDisableFlag ? BRAKE_LIGHT_ENABLE : BRAKE_LIGHT_DISABLE;
+                break;
+            case Acc_Fans_Channel:
+                enableDisableFlag ? ACC_FANS_EN : ACC_FANS_DISABLE;
+                break;
+            case Inverter_Channel:
+                enableDisableFlag ? INVERTER_EN : INVERTER_DISABLE;
+                break;
+            case Radiator_Channel:
+                enableDisableFlag ? RADIATOR_EN : RADIATOR_DISABLE;
+                break;
+            case Aux_1_Channel:
+                enableDisableFlag ? AUX_1_EN : AUX_1_DISABLE;
+                break;
+            case Aux_2_Channel:
+                enableDisableFlag ? AUX_2_EN : AUX_2_DISABLE;
+                break;
+            case Aux_3_Channel:
+                enableDisableFlag ? AUX_3_EN : AUX_3_DISABLE;
+                break;
+            case Aux_4_Channel:
+                enableDisableFlag ? AUX_4_EN : AUX_4_DISABLE;
+                break;
+            default:
+                DEBUG_PRINT("Channel not handled by code\n");
+                break;
+        }
+    }
+}
+
+void configureChannel(Load_Channels channel)
+{
+    // Enable Channel
+    enableDisableChannel(channel, ENABLE_CHANNEL_FLAG);
+
+    // Configure the MUX based on channel
     HAL_GPIO_WritePin(CURR_SENSE_MUX_S1_GPIO_Port, CURR_SENSE_MUX_S1_Pin, ((channel >> 0) & 1) ? GPIO_PIN_SET : GPIO_PIN_RESET);
     HAL_GPIO_WritePin(CURR_SENSE_MUX_S2_GPIO_Port, CURR_SENSE_MUX_S2_Pin, ((channel >> 1) & 1) ? GPIO_PIN_SET : GPIO_PIN_RESET);
     HAL_GPIO_WritePin(CURR_SENSE_MUX_S3_GPIO_Port, CURR_SENSE_MUX_S3_Pin, ((channel >> 2) & 1) ? GPIO_PIN_SET : GPIO_PIN_RESET);
 }
 
-void disableChannel(PDU_Channels_New channel)
+void disableChannel(Load_Channels channel)
 {    
-    // Disable  channel
-    HAL_GPIO_WritePin(MUX_SEL_PORT, MUX_SEL_PIN0, (channel & 0x01) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    // Disable Channel
+    enableDisableChannel(channel, DISABLE_CHANNEL_FLAG);
     
+    // Comments:
     // What if the enabled channel is disabled but the mux is configured to that channel?
     // What is read from MUX_OUT?
 }
 
+void canPublishCurrent(Load_Channels channel, float current) {
+    // for (Load_Channels channel = 0; channel < MAX_NUM_CHANNELS; channel++) {
+    //     switch (channel) {
+    //         // case Pump_1_Channel:
+    //         //     ChannelCurrentFanRight = current;
+    //     }
+    // }
+}
 
 /*********************************************************************************************************************/
 /*-------------------------------------------------------Task--------------------------------------------------------*/
@@ -90,36 +172,40 @@ void loadSensorTask(void *pvParameters)
         Error_Handler();
     }
 
+    // Start Mux ADC Pin
+    if (startADCConversions() != HAL_OK) {
+        ERROR_PRINT("Failed to start ADC conversions\n");
+        Error_Handler();
+    }
+
     // Delay to allow system to turn on
     vTaskDelay(pdMS_TO_TICKS(100));
-
     TickType_t xLastWakeTime = xTaskGetTickCount();
+    
     while (1)
     {
         /* TODO: Add logic for reading current from all channels*/
+        
+        static Load_Channels channel = 1;
 
-        // Example:
-        // One Sequence (one read, one channel)
-        // 1. Enable a load channel (selects which channel I am reading current from). Ex: Set CH_5_EN.
-        // 2. At this point, current on the load channel IC (fuse should be coming through)
-        // 3. Configure the MUX to the channel that is enabled
-        // 4. Read from MUX Out
-        // 5. Update CAN Message and publish it on the bus.
-        // 5. Switch to a different channel and repeat
-
-        static PDU_Channels_New channel = 1;
         // Enable Channel and Configure the Mux Pins
         configureChannel(channel);
 
         // Read from MUX out and update ADC Buffer
-        HAL_ADC_Start_DMA(&ADC_HANDLE, (uint32_t*)ADC_Buffer, NUM_PDU_CHANNELS);
+        // channelCurrent = adcData[0] / ADC_TO_AMPS_DIVIDER;
+
+        float channelCurrent = adcData[0] / ADC_TO_AMPS_DIVIDER;
 
         // Disable Channel
         disableChannel(channel);
-        channel++;
-        }
+
+        // Check if all channels have been iterated, else continue to next channel
+        channel = (channel == MAX_LOAD_CHANNELS) ? 1 : channel + 1;
 
         /* Send CAN Message */
+        canPublishCurrent(channel, channelCurrent);
+
         watchdogTaskCheckIn(LOAD_SENSOR_TASK_ID);
         vTaskDelayUntil(&xLastWakeTime, LOAD_SENSOR_TASK_INTERVAL_MS);
+    }
 }
