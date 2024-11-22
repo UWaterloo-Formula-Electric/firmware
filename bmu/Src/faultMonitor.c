6f84592a8faddf3baf526cba0f46320d3c240e60
Emad Rahman <emad__rahman@hotmail.com>
2024-11-21 21:20:14 -0500

    Invoked new HVIL broken DTC

--- bmu/Src/faultMonitor.c
+++ bmu/Src/faultMonitor.c
@@ -238,6 +238,8 @@ void faultMonitorTask(void *pvParameters)
 			sendCAN_BMU_Interlock_Loop_Status();
 
 			fsmSendEventUrgent(&fsmHandle, EV_HV_Fault, portMAX_DELAY);
+         sendDTC_FATAL_HVIL_Runtime_Error();
+         
 			while (1) {
 				watchdogTaskCheckIn(FAULT_TASK_ID);
 				vTaskDelay(FAULT_MEASURE_TASK_PERIOD);
