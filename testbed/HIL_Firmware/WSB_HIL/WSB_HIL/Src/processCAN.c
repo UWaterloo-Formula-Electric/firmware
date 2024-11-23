b7d961631121368553996c35e0200c48f222dd79
Kevin Liang <kevinliang22069@gmail.com>
2024-11-23 16:01:41 -0500

    project successfully builds

--- testbed/HIL_Firmware/WSB_HIL/WSB_HIL/Src/processCAN.c
+++ testbed/HIL_Firmware/WSB_HIL/WSB_HIL/Src/processCAN.c
@@ -4,15 +4,30 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "driver/twai.h"
-#include "userInit.h"
+#include "../Inc/userInit.h"
 #include "dac.h"
 #include "canReceive.h"
-#include "processCAN.h"
+#include "../Inc/processCAN.h"
 
 void process_rx_task (void * pvParamters) {
     TickType_t xLastWakeTime = xTaskGetTickCount();
 
     while(1) {
+        xQueueReceive(wsb_hil_queue, &can_msg, portMAX_DELAY);
+
+        switch(can_msg.identifier) {
+            case WSBFL_CAN_ID:
+                break;
+            case WSBFR_CAN_ID:
+                break;
+            case WSBRL_CAN_ID:
+                break;
+            case WSBRR_CAN_ID:
+                break;
+            default:
+                printf("CAN ID not recognized %ld\r\n", can_msg.identifier);
+                break;
+        }
 
         vTaskDelayUntil(&xLastWakeTime, PROCESS_RX_TASK_INTERVAL_MS);
     }
--- testbed/HIL_Firmware/WSB_HIL/WSB_HIL/Src/userInit.c
+++ testbed/HIL_Firmware/WSB_HIL/WSB_HIL/Src/userInit.c
@@ -7,10 +7,10 @@
 #include "esp_err.h"
 #include "driver/twai.h"
 #include "driver/spi_master.h"
-#include "userInit.h"
+#include "../Inc/userInit.h"
 #include "dac.h"
 #include "canReceive.h"
-
+#include "../Inc/processCAN.h"
 
 void taskRegister (void)
 {
@@ -96,9 +96,8 @@ esp_err_t CAN_init (void) {
 
 }
 
-esp_err_t spi_init(void)
-{
-    
+esp_err_t spi_init(void) {
+    return ESP_OK;
 }
 
 
