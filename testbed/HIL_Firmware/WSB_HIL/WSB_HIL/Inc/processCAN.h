d9a8dc70f0aeeb338f43f48985fe72ad52edc4a1
Kevin Liang <kevinliang22069@gmail.com>
2024-11-29 03:24:19 -0500

    wrote CAN message processing code, awaiting spi init finish

--- testbed/HIL_Firmware/WSB_HIL/WSB_HIL/Inc/processCAN.h
+++ testbed/HIL_Firmware/WSB_HIL/WSB_HIL/Inc/processCAN.h
@@ -1,6 +1,8 @@
 #ifndef PROCESS_CAN_H
 #define PROCESS_CAN_H
 
+#include <stdio.h>
+
 #define PROCESS_RX_TASK_INTERVAL_MS 1
 
 //TODO: implement more sensor signals as they are integrated
@@ -11,10 +13,22 @@
  */
 
 #define WSBFL_CAN_ID 0x400080F
-#define WSBFR_CAN_ID 0x400090F //todo: receiver needs mapping after signal added
+#define WSBFR_CAN_ID 0x400090F //todo: receiver needs mapping after adding first signal
 #define WSBRL_CAN_ID 0x4000A0F
 #define WSBRR_CAN_ID 0x4000B0F
 
+#define WSBFL_MSG_LENGTH 2
+#define WSBFR_MSG_LENGTH 0
+#define WSBRL_MSG_LENGTH 1
+#define WSBRR_MSG_LENGTH 3
+
 void process_rx_task (void * pvParamters);
+void processFL(twai_message_t* can_msg);
+void processFR(twai_message_t* can_msg);
+void processRL(twai_message_t* can_msg);
+void processRR(twai_message_t* can_msg);
+void input_data(twai_message_t* can_msg, uint8_t msg_length);
+
+//todo: add board identifier variable?
 
 #endif/*PROCESS_CAN_H*/
--- testbed/HIL_Firmware/WSB_HIL/WSB_HIL/Src/processCAN.c
+++ testbed/HIL_Firmware/WSB_HIL/WSB_HIL/Src/processCAN.c
@@ -9,6 +9,7 @@
 #include "canReceive.h"
 #include "../Inc/processCAN.h"
 
+//reassembles data into 16 bit uint to pass in desired values to dac & pwm functions
 void process_rx_task (void * pvParamters) {
     TickType_t xLastWakeTime = xTaskGetTickCount();
 
@@ -17,12 +18,16 @@ void process_rx_task (void * pvParamters) {
 
         switch(can_msg.identifier) {
             case WSBFL_CAN_ID:
+                processFL(&can_msg);
                 break;
             case WSBFR_CAN_ID:
+                processFR(&can_msg);
                 break;
             case WSBRL_CAN_ID:
+                processRL(&can_msg);
                 break;
             case WSBRR_CAN_ID:
+                processRR(&can_msg);
                 break;
             default:
                 printf("CAN ID not recognized %ld\r\n", can_msg.identifier);
@@ -30,5 +35,49 @@ void process_rx_task (void * pvParamters) {
         }
 
         vTaskDelayUntil(&xLastWakeTime, PROCESS_RX_TASK_INTERVAL_MS);
+    }
+}
+
+static uint8_t bytes[8];
+static uint16_t brake_ir = 0U;
+static uint16_t hall_effect = 0U;
+
+void processFL(twai_message_t* can_msg) {
+    input_data(can_msg, WSBFL_MSG_LENGTH);
+
+    brake_ir = bytes[1] << 8;
+    brake_ir |= bytes[0];
+    //todo: set voltage (blocked)
+}
+
+void processFR(twai_message_t* can_msg) {
+    input_data(can_msg, WSBFR_MSG_LENGTH);
+}
+
+void processRL(twai_message_t* can_msg) {
+    input_data(can_msg, WSBRL_MSG_LENGTH);
+
+    hall_effect = bytes[0];
+    //todo: set pwm (blocked)
+}
+
+void processRR(twai_message_t* can_msg) {
+    input_data(can_msg, WSBRR_MSG_LENGTH);
+
+    brake_ir = bytes[1] << 8;
+    brake_ir |= bytes[0];
+    //todo: set voltage (blocked)
+    hall_effect = bytes[2];
+    //todo: set pwm (blocked)
+}
+
+void input_data(twai_message_t* can_msg, uint8_t msg_length) {
+    if (msg_length > 8) {
+        printf("Message length too long");
+        return;
+    }
+
+    for (int i = 0; i < msg_length; ++i) {
+        bytes[i] = can_msg->data[i];
     }
 }
