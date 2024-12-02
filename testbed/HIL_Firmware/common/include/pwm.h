a6dccf57aeb3088142a0b4848b3ce890e327c9cd
Kevin Liang <kevinliang22069@gmail.com>
2024-12-01 20:07:48 -0500

    pwm init for hall effect

---
+++ testbed/HIL_Firmware/common/include/pwm.h
@@ -0,0 +1,8 @@
+#ifndef INC_2024_WSB_HIL_PWM_H
+#define INC_2024_WSB_HIL_PWM_H
+#include "esp_err.h"
+#include "driver/ledc.h" //api for pwm outputs
+
+esp_err_t encoder_switch_directions();
+
+#endif //INC_2024_WSB_HIL_PWM_H
