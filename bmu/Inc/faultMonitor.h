0f56f20aaa562163a65a2333b628bcf4d54a630d
Daniel Bishara <d2bishar@uwaterloo.ca>
2024-12-16 21:24:44 -0500

    update fault monitor

--- bmu/Inc/faultMonitor.h
+++ bmu/Inc/faultMonitor.h
@@ -4,13 +4,27 @@
 
 #include "bsp.h"
 
+typedef enum faultMonitorBits_e {
+    BOTS_FAILED_BIT = 0,
+    EBOX_FAILED_BIT,
+    BSPD_FAILED_BIT,  
+    HVD_FAILED_BIT,
+    AMS_FAILED_BIT,
+    IMD_FAILED_BIT, 
+    CBRB_FAILED_BIT,   
+    TSMS_FAILED_BIT,
+    HW_CHECK_FAILED_BIT,
+    FSM_STATE_BIT,
+}faultMonitorBits_e;
 
-bool getHVIL_Status();
-bool getIL_Status();
+bool getBOTS_Status();
+bool getEbox_Il_Status();
 bool getBSPD_Status();
-bool getTSMS_Status();
 bool getHVD_Status();
-bool getIL_BRB_Status();
-bool getCBRB_IL_Status();
+bool getAMS_Status();
+bool getIMD_Status();
+bool getCBRB_Status();
+bool getTSMS_Status();
+bool getHwCheck_Status();
 
 #endif /* end of include guard: FAULTMONITOR_H */
