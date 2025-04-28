01ce8b9a7dc63c6b8e5890d5acf65fef9c95822c
I am the Bay Laptop <uwaterlooelectric@gmail.com>
2025-04-27 23:32:40 -0400

    fix bug in BMY dtc callback

--- vcu/Src/canReceive.c
+++ vcu/Src/canReceive.c
@@ -141,6 +141,7 @@ void CAN_Msg_BMU_DTC_Callback(int DTC_CODE, int DTC_Severity, int DTC_Data) {
         case FATAL_IMD_Failure:
             ERROR_PRINT_ISR("Got IMD failure\n");
             IMD_FAIL_LED_ON
+            break;
         case CRITICAL_CELL_VOLTAGE_LOW: // All 4 cases fall through
         case CRITICAL_CELL_VOLTAGE_HIGH:
         case CRITICAL_CELL_TEMP_HIGH:
