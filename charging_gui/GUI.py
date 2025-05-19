184c97beab6477462fc7030914c30b495d6e660e
Zhenfeng Chen <z835chen@uwaterloo.ca>
2025-05-19 16:53:50 -0400

    created files for reading CAN data and creating the GUI.

---
+++ charging_gui/GUI.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+root = tk.Tk(screenName = "Battery Cells Charging")
+
+root.mainloop()
