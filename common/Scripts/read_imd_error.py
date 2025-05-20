6a9a7d68a665bbac5ee54e49483c68b3794bde6a
Emad Rahman <emad__rahman@hotmail.com>
2025-05-19 22:48:14 -0400

    Updated python script

--- common/Scripts/read_imd_error.py
+++ common/Scripts/read_imd_error.py
@@ -1,20 +1,23 @@
 import cantools
 import can
 import time
+from can.interfaces.vector import VectorBus
 
 db = cantools.database.load_file("common/Data/2024CAR.dbc")
-can_bus = can.interface.Bus('vcan0', bustype='socketcan')
+can_bus = VectorBus(channel=1)
 message = db.get_message_by_name('Read_IMD_Error_Threshold')
-signal_dict = {'IMD_Error_Threshold': 100}
+target_id = message.frame_id
+signal_dict = {'IMD_Index': 0x46}
 data = message.encode(signal_dict)
 msg = can.Message(arbitration_id=message.frame_id, data=data, is_extended_id=message.is_extended_frame)
 
 can_bus.send(msg)
-time.sleep(0.5)
 
-message = can_bus.recv()
-decoded = message.decode(message.data)
-print(decoded)
+while True:
+    message = can_bus.recv()
+    if message.arbitration_id == target_id:
+        decoded = db.decode_message(message.arbitration_id, message.data)
+        print(decoded)
 
 
 
