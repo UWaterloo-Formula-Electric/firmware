bd45b9b516cc125e08275f72b90ccad93f0775eb
Zhenfeng Chen <z835chen@uwaterloo.ca>
2025-05-22 19:31:21 -0400

    integrated the CAN data reading function to the GUI

--- charging_gui/GUI.py
+++ charging_gui/GUI.py
@@ -1,94 +1,110 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, QGroupBox, QGridLayout
-import numpy as np
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QGridLayout
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFont
 import pyqtgraph as pg
+from readCAN import CANBMUMonitor
 
-# simulate real time data for now
-# replace with real time CAN data later
-labels = []
-voltage_threshold = 3.0
-temp_threshold_h = 45
-temp_threshold_l = 30
 
-def update():
-    voltages = np.round(np.random.uniform(3.0, 4.2, 140), 1)
-    temperatures = np.round(np.random.uniform(0, 60, 140), 1)
-    for index, label in enumerate(labels):
-        voltage = voltages[index]
-        temperature = temperatures[index]
-        if voltage > voltage_threshold:
-            voltage_color = "green"
-        else:
-            voltage_color = "red"
 
-        if temperature > temp_threshold_h:
-            temperature_color = "red"
-        elif temperature > temp_threshold_l:
-            temperature_color = 'yellow'
-        else:
-            temperature_color = 'green'
+class GUI():
+    def __init__(self, BMU_MONITOR):
+        self.BMU_MONITOR = BMU_MONITOR
+        self.voltage_threshold = 3.0
+        self.temp_threshold_h = 45
+        self.temp_threshold_l = 30
+        self.update_time = 1000
 
-        label.setText(
-                f"<span style='color: {voltage_color};'>{voltages[index]} V</span><br>"
-                f"<span style='color: {temperature_color};'>{temperatures[index]}°C</span>"
-        )        
+        # set up the widget for the GUI
+        self.labels = []
+        self.app = QApplication([])
+        self.window = QWidget()
+        self.window.setWindowTitle("Charging GUI")
+        self.window.setGeometry(100, 100, 1000, 700)
+        self.layout = QGridLayout()
+        self.window.setLayout(self.layout)
+
+        self.add_boxes()
+        self.timer = pg.QtCore.QTimer()
+        # refresh every second
+        self.timer.timeout.connect(self.update)
+        self.timer.start(self.update_time)
+
+
+
+
+    def add_boxes(self):
+
+        # use generated data to test for now 
+        self.BMU_MONITOR.get_message()
+        self.BMU_MONITOR.process_can_message()
+        voltages, temperatures = self.BMU_MONITOR.voltage_channels, self.BMU_MONITOR.temperature_channels
+
+        for i in range(140):
+            row = i%20
+            col = i // 20
+            voltage = voltages[i]
+            voltage_color = None
+            temperature = temperatures[i]
+            temperature_color = None
+            # setting voltage/temperature color
+            if voltage > self.voltage_threshold:
+                voltage_color = "green"
+            else:
+                voltage_color = "red"
+
+            if temperature > self.temp_threshold_h:
+                temperature_color = "red"
+            elif temperature > self.temp_threshold_l:
+                temperature_color = 'yellow'
+            else:
+                temperature_color = 'green'
+
+            label = QLabel()
+            label.setText(
+                f"<span style='color: {voltage_color};'>{voltages[i]} V</span><br>"
+                f"<span style='color: {temperature_color};'>{temperatures[i]}°C</span>"
+            )
+            label.setFixedSize(150, 30)
+            label.setStyleSheet("border: 1px solid black; background-color: black;")
+            label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            label.setFont(QFont("Arial", 8))
+            self.labels.append(label)
+            self.layout.addWidget(label, row, col)
+        
+
+    def update(self):
+        self.BMU_MONITOR.get_message()
+        self.BMU_MONITOR.process_can_message()
+        voltages, temperatures = self.BMU_MONITOR.voltage_channels, self.BMU_MONITOR.temperature_channels
+        for index, label in enumerate(self.labels):
+            voltage = voltages[index]
+            temperature = temperatures[index]
+            if voltage > self.voltage_threshold:
+                voltage_color = "green"
+            else:
+                voltage_color = "red"
+
+            if temperature > self.temp_threshold_h:
+                temperature_color = "red"
+            elif temperature > self.temp_threshold_l:
+                temperature_color = 'yellow'
+            else:
+                temperature_color = 'green'
+
+            label.setText(
+                    f"<span style='color: {voltage_color};'>{voltages[index]} V</span><br>"
+                    f"<span style='color: {temperature_color};'>{temperatures[index]}°C</span>"
+            )    
+
+    def start(self):
+        self.window.show()
+        self.app.exec()
+
 
 def main():
-    app = QApplication([])
-
-    window = QWidget()
-    window.setWindowTitle("Charging GUI")
-    window.setGeometry(100, 100, 1000, 700)
-    layout = QGridLayout()
-    window.setLayout(layout)
-
-    x = np.arange(140)  
-    voltages = np.round(np.random.uniform(0, 6.55, 140), 1)
-    temperatures = np.round(np.random.uniform(20, 40, 140), 1)
-
-    
-    for i in range(140):
-        row = i%20
-        col = i // 20
-        voltage = voltages[i]
-        voltage_color = None
-        temperature = temperatures[i]
-        temperature_color = None
-        # setting voltage color
-        if voltage > voltage_threshold:
-            voltage_color = "green"
-        else:
-            voltage_color = "red"
-
-        if temperature > temp_threshold_h:
-            temperature_color = "red"
-        elif temperature > temp_threshold_l:
-            temperature_color = 'yellow'
-        else:
-            temperature_color = 'green'
-
-        label = QLabel()
-        label.setText(
-            f"<span style='color: {voltage_color};'>{voltages[i]} V</span><br>"
-            f"<span style='color: {temperature_color};'>{temperatures[i]}°C</span>"
-        )
-        label.setFixedSize(150, 30)
-        label.setStyleSheet("border: 1px solid black; background-color: black;")
-        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        label.setFont(QFont("Arial", 8))
-        labels.append(label)
-        layout.addWidget(label, row, col)
-    
-
-   
-
-    timer = pg.QtCore.QTimer()
-    timer.timeout.connect(update)
-    timer.start(1000)
-# update the GUI every second
-    window.show()
-    app.exec()
+    monitor = CANBMUMonitor()
+    gui = GUI(monitor)
+    gui.start()
 
 main()
 
