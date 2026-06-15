0a833c20e8553c87786ec2c0025b356c9d197240
MohilKadakia <30442978+MohilKadakia@users.noreply.github.com>
2026-06-15 04:37:45 -0400

    Resolve merge conflict in batt_read_thermistors thermistor conversion

--- bmu/Src/F7_Src/adbms6830.c
+++ bmu/Src/F7_Src/adbms6830.c
@@ -857,14 +857,8 @@ HAL_StatusTypeDef batt_read_thermistors(size_t channel, float *cell_temp_array) 
 
 			// Convert ADC code to volts
 			// From Table 104: GPIO Voltage = ADC × 150 uV + 1.5 V
-<<<<<<< pcdc_cell_safety
 			float voltageThermistor = (adcCounts * 0.000150f) + 1.5f + 0.06f;
 			cell_temp_array[tempIdx] = batt_convert_voltage_to_temp(voltageThermistor);
-=======
-			float voltageThermistor = (adcCounts * 0.000150f) + 1.5f;
-			cell_temp_array[tempIdx] = batt_thermistor_adc_to_temp((int)tempIdx, voltageThermistor);
-    
->>>>>>> michigan_26
 		}
 	}
 	return HAL_OK;
