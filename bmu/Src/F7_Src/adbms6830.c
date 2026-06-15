6ff1c761e1d767deabe664d2e039fdc7dd1bd1e7
Emad Rahman <emad__rahman@hotmail.com>
2026-06-14 22:43:59 -0400

    Fixing stopBalance (inshAllah)

--- bmu/Src/F7_Src/adbms6830.c
+++ bmu/Src/F7_Src/adbms6830.c
@@ -858,8 +858,7 @@ HAL_StatusTypeDef batt_read_thermistors(size_t channel, float *cell_temp_array) 
 			// Convert ADC code to volts
 			// From Table 104: GPIO Voltage = ADC × 150 uV + 1.5 V
 			float voltageThermistor = (adcCounts * 0.000150f) + 1.5f + 0.06f;
-			//cell_temp_array[tempIdx] = batt_convert_voltage_to_temp(voltageThermistor);
-			cell_temp_array[tempIdx] = voltageThermistor;
+			cell_temp_array[tempIdx] = batt_convert_voltage_to_temp(voltageThermistor);
 		}
 	}
 	return HAL_OK;
